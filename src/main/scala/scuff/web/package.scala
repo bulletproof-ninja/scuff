b0f34db6ad558cc5dfba9494a14c00927a91c1be
nilskp <nilskp@gmail.com>
2013-01-16 08:50:20 -0600

    Add date format factory method

--- src/main/scala/scuff/web/package.scala
+++ src/main/scala/scuff/web/package.scala
@@ -2,8 +2,10 @@ package scuff
 
 package object web {
   final val LastModified = "Last-Modified"
+  final val ContentLength = "Content-Length"
   final val ETag = "ETag"
   final val Age = "Age"
   final val IfNoneMatch = "If-None-Match"
   final val IfModifiedSince = "If-Modified-Since"
+  def RFC822 = new java.text.SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z", java.util.Locale.US)
 }
