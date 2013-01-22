2efa442cd63bd5afecb1f3780ee15789b9d44944
nilskp <nilskp@gmail.com>
2013-01-22 10:39:31 -0600

    Add general Transformer interface, with Serializer sub interface

--- src/main/scala/scuff/Transformer.scala
+++ src/main/scala/scuff/Transformer.scala
@@ -7,20 +7,31 @@ trait Transformer[A, B] {
   def back(b: B): A
 }
 
-trait Serializer[T] extends Transformer[T, Array[Byte]] {
-  def forth(t: T): Array[Byte] = {
+trait Serializer[T] extends Transformer[T, Array[Byte]]
+
+trait StreamingSerializer[T] extends Serializer[T] {
+  @inline def forth(t: T): Array[Byte] = {
     val out = new ByteArrayOutputStream
-    streamForth(t, out)
+    streamForth(out)(t)
     out.close()
     out.toByteArray()
   }
-  def back(bytes: Array[Byte]) = streamBack(new ByteArrayInputStream(bytes))
-
-  def streamForth(t: T, out: OutputStream)
+  @inline def back(bytes: Array[Byte]) = streamBack(new ByteArrayInputStream(bytes))
+  @inline protected final def asObjectInput(in: InputStream): ObjectInput = in match {
+    case oi: ObjectInput ⇒ oi
+    case _ ⇒ new ObjectInputStream(in)
+  }
+  @inline protected final def asObjectOutput(out: OutputStream): ObjectOutput = out match {
+    case ou: ObjectOutput ⇒ ou
+    case _ ⇒ new ObjectOutputStream(out)
+  }
+  def streamForth(out: OutputStream)(t: T)
   def streamBack(in: InputStream): T
 }
 
-class JavaSerializer[T] extends Serializer[T] {
-  def streamForth(obj: T, out: OutputStream) = new ObjectOutputStream(out).writeObject(obj)
-  def streamBack(in: InputStream): T = new ObjectInputStream(in).readObject().asInstanceOf[T]
+class JavaSerializer[T] extends StreamingSerializer[T] {
+  @inline def streamForth(out: OutputStream)(obj: T) = asObjectOutput(out).writeObject(obj)
+  @inline def streamBack(in: InputStream): T = asObjectInput(in).readObject().asInstanceOf[T]
 }
+
+object JavaSerializer extends JavaSerializer[Any]
