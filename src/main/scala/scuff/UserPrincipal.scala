c515b18cae81aa31f4acb81474084f23231ac382
nilskp <nilskp@gmail.com>
2013-01-25 20:26:49 -0600

    Add UserPrincipal class

---
+++ src/main/scala/scuff/UserPrincipal.scala
@@ -0,0 +1,14 @@
+package scuff
+
+import java.security.Principal
+
+case class UserPrincipal(login: String, roles: Set[String]) extends Principal {
+  def getName = login
+  override def toString = "%s:%s".format(login, roles)
+  override def equals(any: Any) = any match {
+    case that: Principal ⇒ this.getName == that.getName
+    case _ ⇒ false
+  }
+  override def hashCode = login.hashCode
+}
+
