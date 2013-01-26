d20dfc853d58bde18d6896597adf08172462ee65
nilskp <nilskp@gmail.com>
2013-01-26 13:46:41 -0600

    Distributed session using Redis.

---
+++ src/main/scala/scuff/web/RedisHttpSessionFilter.scala
@@ -0,0 +1,64 @@
+package scuff.web
+
+import scuff.redis.RedisConnectionPool
+import javax.servlet._
+import javax.servlet.http._
+import scuff.redis.RedisStringKeyHashMap
+import scuff.JavaSerializer
+
+/**
+  * Given a specific Redis database, this filter stores any session
+  * data in Redis, making it available in a horizontally
+  * scaled environment.
+  */
+trait RedisHttpSessionFilter extends HttpFilter {
+
+  def redisSessionDB: RedisConnectionPool
+
+  def doFilter(req: HttpServletRequest, res: HttpServletResponse, chain: FilterChain) {
+    val request = new HttpServletRequestWrapper(req) {
+      private[this] lazy val session = new RedisHttpSession(redisSessionDB, req.getSession)
+      override def getSession = session
+      override def getSession(create: Boolean) =
+        if (create || req.getSession(false) != null) {
+          session
+        } else {
+          null
+        }
+    }
+    chain.doFilter(request, res)
+  }
+}
+
+class RedisHttpSession(redis: RedisConnectionPool, delegate: HttpSession) extends HttpSession {
+  import scuff.redis._
+  import collection.JavaConverters._
+  private[this] var _map: RedisStringKeyHashMap[AnyRef] = _
+  private def map = {
+    if (_map == null) {
+      _map = threadSafe(redis)(new RedisStringKeyHashMap(delegate.getId, _, JavaSerializer))
+    }
+    _map
+  }
+  def getAttribute(attr: String) = map.getOrElse(attr, null)
+  def getAttributeNames() = map.keysIterator.asJavaEnumeration
+  def getCreationTime() = delegate.getCreationTime()
+  def getId() = delegate.getId()
+  val getLastAccessedTime = System.currentTimeMillis()
+  def getMaxInactiveInterval() = delegate.getMaxInactiveInterval()
+  def getServletContext() = delegate.getServletContext()
+  def invalidate() = {
+    delegate.invalidate()
+    if (_map != null) _map.clear()
+  }
+  def isNew() = delegate.isNew()
+  def removeAttribute(attr: String) = map.del(attr)
+  def setAttribute(attr: String, value: AnyRef) = map.set(attr, value)
+  def setMaxInactiveInterval(max: Int) = delegate.setMaxInactiveInterval(max)
+
+  def getSessionContext() = sys.error("Deprecated and you know it")
+  def getValue(name: String) = sys.error("Deprecated and you know it")
+  def getValueNames() = sys.error("Deprecated and you know it")
+  def putValue(n: String, v: Object) = sys.error("Deprecated and you know it")
+  def removeValue(n: String) = sys.error("Deprecated and you know it")
+}
