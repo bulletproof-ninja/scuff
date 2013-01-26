458852adc80c1b16e10622a8976b5f07784158a4
nilskp <nilskp@gmail.com>
2013-01-25 20:29:02 -0600

    Add convenience class for String keys

---
+++ src/main/scala/scuff/redis/RedisStringKeyMap.scala
@@ -0,0 +1,7 @@
+package scuff.redis
+
+import redis.clients.jedis._
+import redis.clients.util.SafeEncoder._
+
+class RedisStringKeyMap[V](connection: CONNECTION, serializer: scuff.Serializer[V] = new scuff.JavaSerializer[V])
+  extends RedisMap[String, V](connection, StringKeySerializer, serializer)
