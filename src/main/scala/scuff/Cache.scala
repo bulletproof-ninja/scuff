479a4f2bab2edffa881fc751ef6d45a467c7fea1
nilskp <nilskp@gmail.com>
2013-01-26 13:43:36 -0600

    Docs

--- src/main/scala/scuff/Cache.scala
+++ src/main/scala/scuff/Cache.scala
@@ -1,45 +1,44 @@
 package scuff
 
 /**
- * Generic cache interface.
- */
+  * Generic cache interface.
+  */
 trait Cache[K, V] {
   /**
-   * Default time-to-live, in seconds. 
-   * Can be overridden per entry when storing.
-   * 0 means no expiry.
-   */
+    * Default time-to-live, in seconds.
+    * Can be overridden per entry when storing.
+    * 0 means no expiry.
+    */
   def defaultTTL: Int
   /**
-   * Store or replace value.
-   * @param key Cache key
-   * @param value Value to cache
-   * @param ttl Time-to-live. Optional. Defaults to [[defaultTTL]].
-   */
+    * Store or replace value.
+    * @param key Cache key
+    * @param value Value to cache
+    * @param ttl Time-to-live. Optional. Defaults to [[defaultTTL]].
+    */
   def store(key: K, value: V, ttlSeconds: Int = defaultTTL)
-  
+
   /**
-   * Evict cache entry.
-   */
+    * Evict cache entry.
+    */
   def evict(key: K): Option[V]
-  
+
   /**
-   * Lookup cache entry.
-   */
+    * Lookup cache entry.
+    */
   def lookup(key: K): Option[V]
 
   /**
-   * Lookup cache entry, or if not found, construct, store, and return entry.
-   * @param key
-   * @param constructor The value constructor, if key not found
-   * @param ttlSeconds time-to-live if stored. Defaults to [[defaultTTL]]
-   */
-  def lookupOrStore(key: K, ttlSeconds: Int = defaultTTL)(constructor: => V): V
-  
+    * Lookup cache entry, or if not found, construct, store, and return entry.
+    * @param key
+    * @param constructor The value constructor, if key not found
+    * @param ttlSeconds time-to-live if stored. Defaults to [[defaultTTL]]
+    */
+  def lookupOrStore(key: K, ttlSeconds: Int = defaultTTL)(constructor: ⇒ V): V
+
   /**
-   * Disable cache. This will permanently disable the cache and purge all entries.
-   * Further attempts to use the cache will result in a [[IllegalStateException]].
-   */
-  @throws(classOf[IllegalStateException])
+    * Disable cache. This will permanently disable the cache and purge all entries.
+    * Further attempts to use the cache will result in a [[IllegalStateException]].
+    */
   def disable()
 }
