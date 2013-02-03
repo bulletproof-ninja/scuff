3bd0c76eee436969d7a01e7a9332738f49128bcc
nilskp <nilskp@gmail.com>
2013-02-03 10:56:24 -0600

    Handle everything. Less performant, but easier. So build custom if performance is a problem.

--- src/main/scala/scuff/es/util/MongoEventConsumer.scala
+++ src/main/scala/scuff/es/util/MongoEventConsumer.scala
@@ -7,27 +7,41 @@ import scuff.Mongolia._
   * Keep track of handled [[Transaction]]s, so process can resume
   * after shutdown.
   */
-abstract class MongoEventConsumer[ID, EVT](
-    coll: DBCollection,
+abstract class MongoEventConsumer[ID <% BsonValue, EVT](
+    dbColl: DBCollection,
     txnDocID: BsonValue) extends scuff.es.PersistentEventConsumer[ID, EVT] {
 
-  private[this] val dbColl = enrich(coll)
+  protected def writeConcern: WriteConcern = WriteConcern.UNACKNOWLEDGED
+
+  private[this] val syncLock = new AnyRef
 
   private[this] val txnDoc = {
     val doc = obj("_id" := txnDocID)
     dbColl.findOpt(doc).getOrElse(doc)
   }
+  private[this] val revDocSave = obj()
+  private[this] val revDocQry = obj()
 
-  final def lastProcessedTxn: Option[BigInt] = txnDoc.synchronized {
+  final def lastProcessedTxn: Option[BigInt] = syncLock.synchronized {
     txnDoc("last").opt[Array[Byte]].map(BigInt(_))
   }
 
-  protected def next(txn: TXN)
+  final def lastProcessedRev(streamId: ID): Option[Long] = syncLock.synchronized {
+    revDocQry += ("_id" := streamId)
+    dbColl.findOne(revDocQry) match {
+      case null ⇒ None
+      case doc ⇒ Some(doc.getAs[Long]("rev"))
+    }
+  }
 
-  final def consume(txn: TXN) = txnDoc.synchronized {
-    next(txn)
+  protected def processNext(txn: TXN)
+
+  final def consume(txn: TXN) = syncLock.synchronized {
+    processNext(txn)
+    revDocSave.add("_id" := txn.streamId, "rev" := txn.revision)
+    dbColl.save(revDocSave, writeConcern)
     txnDoc.put("last", txn.transactionID.toByteArray)
-    dbColl.save(txnDoc)
+    dbColl.save(txnDoc, writeConcern)
   }
 
 }
