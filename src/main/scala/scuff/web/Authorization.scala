38944133f1aae3030d7b5424e37278aba7e7df5e
nilskp <nilskp@gmail.com>
2013-02-07 12:12:54 -0500

    Refactor auth strategy

--- src/main/scala/scuff/web/Authorization.scala
+++ src/main/scala/scuff/web/Authorization.scala
@@ -6,62 +6,89 @@ import java.security.Principal
 import HttpServletResponse._
 import scuff.UserPrincipal
 
+/**
+ * Application authorization trait. Apply to servlets
+ * to limit access.
+ */
 trait Authorization extends HttpServlet {
 
   /** Roles allowed for this servlet */
   protected def rolesAllowed: Set[String]
 
-  /**
-    * Lookup authenticated user by session ID.
-    * @return Authenticated user, or None if not authenticated
-    */
-  protected def getAuthenticatedUser(sessionID: String): Option[UserPrincipal]
-
   abstract override def service(req: HttpServletRequest, res: HttpServletResponse) {
-    req.getSession(false) match {
+    req.getUserPrincipal match {
       case null ⇒ res.setStatus(SC_UNAUTHORIZED)
-      case session ⇒
-        getAuthenticatedUser(session.getId) match {
-          case None ⇒ res.setStatus(SC_UNAUTHORIZED)
-          case Some(user) ⇒
-            if (rolesAllowed.intersect(user.roles).isEmpty) {
-              res.setStatus(SC_FORBIDDEN)
-            } else {
-              val reqProxy = new HttpServletRequestWrapper(req) {
-                override def isUserInRole(role: String) = user.roles.contains(role)
-                override def getUserPrincipal = user
-              }
-              super.service(reqProxy, res)
-            }
+      case user ⇒
+        if (rolesAllowed.exists(req.isUserInRole)) {
+          super.service(req, res)
+        } else {
+          res.setStatus(SC_FORBIDDEN)
         }
-
     }
   }
 
 }
 
-abstract class AuthenticationRerouteFilter extends HttpFilter {
+/**
+ * Augment HttpServletRequest with principal and roles.
+ */
+abstract class ApplicationSecurityFilter extends HttpFilter {
+  /**
+   * Lookup authenticated user by session ID.
+   * @return Authenticated user, or None if not authenticated
+   */
+  protected def getAuthenticatedUser(sessionID: String): Option[UserPrincipal]
+  protected def logoutUser(sessionID: String)
 
-  val Mode = new Enumeration {
-    val Redirect, Forward = Value
+  def doFilter(req: HttpServletRequest, res: HttpServletResponse, chain: FilterChain) {
+    require(req.getUserPrincipal == null, "Another authentication mechanism is already being used: " + req.getUserPrincipal)
+    val request = req.getSession(false) match {
+      case null ⇒ req
+      case session ⇒
+        getAuthenticatedUser(session.getId) match {
+          case None ⇒ req
+          case Some(user) ⇒
+            new HttpServletRequestWrapper(req) {
+              @volatile var loggedOut = false
+              override def isUserInRole(role: String) = !loggedOut && user.roles.contains(role)
+              override def getUserPrincipal = if (loggedOut) null else user
+              override def logout {
+                logoutUser(session.getId)
+                super.logout()
+                loggedOut = true
+              }
+              override def getAuthType = classOf[Authorization].getName
+              override def getRemoteUser = if (loggedOut) null else user.getName
+            }
+        }
+    }
+    chain.doFilter(request, res)
   }
+}
+
+/**
+ * Apply this trait to an existing filter to get forwarding
+ * (not redirect) to login page.
+ */
+trait AuthenticationForwarding extends HttpFilter {
 
   protected def loginPage: String
-  protected def mode: Mode.Value
 
-  protected def doFilter(req: HttpServletRequest, res: HttpServletResponse, chain: FilterChain) {
-    chain.doFilter(req, res)
+  abstract override def doFilter(req: HttpServletRequest, res: HttpServletResponse, chain: FilterChain) {
+    super.doFilter(req, res, chain)
     if (!res.isCommitted) res.getStatus match {
-      case SC_UNAUTHORIZED ⇒ mode match {
-        case Mode.Forward ⇒
-          res.setStatus(SC_FOUND)
-          req.getRequestDispatcher(loginPage).forward(req, res)
-        case Mode.Redirect ⇒
-          res.sendRedirect(req.getContextPath concat loginPage)
-      }
+      case SC_UNAUTHORIZED ⇒
+        res.setStatus(SC_FOUND)
+        req.getRequestDispatcher(loginPage).forward(req, res)
       case SC_FORBIDDEN ⇒ res.sendError(SC_FORBIDDEN)
       case _ ⇒ // Ignore anything else
     }
   }
 
 }
+
+/**
+ * Standalone application of AuthenticationForwarding trait.
+ * Does nothing else.
+ */
+abstract class AuthenticationForwardingFilter extends NoOpHttpFilter with AuthenticationForwarding
