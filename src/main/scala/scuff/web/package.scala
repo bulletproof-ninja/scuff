11bb0677f920c979c41b7fa476b974ce1dcc8e0a
nilskp <nilskp@gmail.com>
2013-02-28 10:20:57 -0600

    SimpleDateFormat is expensive, so keep in ThreadLocal

--- src/main/scala/scuff/web/package.scala
+++ src/main/scala/scuff/web/package.scala
@@ -7,5 +7,8 @@ package object web {
   final val Age = "Age"
   final val IfNoneMatch = "If-None-Match"
   final val IfModifiedSince = "If-Modified-Since"
-  def RFC822 = new java.text.SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z", java.util.Locale.US)
+  private val RFC822Pool = new ThreadLocal[java.text.SimpleDateFormat] {
+    override def initialValue = new java.text.SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss Z", java.util.Locale.US)
+  }
+  def RFC822 = RFC822Pool.get()
 }
