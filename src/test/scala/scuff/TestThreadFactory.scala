259ab921b0c0d4603665a6470b3dbe97f70d10d4
nilskp <nilskp@gmail.com>
2013-04-24 22:22:22 -0500

    Tests for thread factory

---
+++ src/test/scala/scuff/TestThreadFactory.scala
@@ -0,0 +1,21 @@
+package scuff
+
+import org.junit._
+import org.junit.Assert._
+import java.util.concurrent._
+
+class TestThreadFactory extends Serializable {
+  @Test
+  def foo {
+    val tf = ThreadFactory("MyThread")
+    val latch = new CountDownLatch(1)
+    val thread = tf newThread new Runnable {
+      def run = latch.countDown()
+    }
+    assertEquals("MyThread[0]", thread.getName)
+    assertEquals("MyThread", thread.getThreadGroup.getName)
+    thread.start()
+    assertTrue(latch.await(2, TimeUnit.SECONDS))
+  }
+
+}
