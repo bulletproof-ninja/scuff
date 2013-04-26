31b013ca23fe59f7c46243fbf735c3c163f983a3
nilskp <nilskp@gmail.com>
2013-04-25 20:03:55 -0500

    Add explicit exception handling, which is otherwise often an oversight.

--- src/main/scala/scuff/FuzzyScheduler.scala
+++ src/main/scala/scuff/FuzzyScheduler.scala
@@ -6,9 +6,33 @@ import javax.servlet._
 object FuzzyScheduler {
   trait FuzzyRunnable extends Runnable {
     @volatile private[this] var alive = true
+    /**
+      * Handle exception.
+      * Rethrow to bubble up to underlying executor service
+      * (NOTE that this will potentially affect other jobs),
+      * or call `stop()` to stop orderly. Or do something
+      * else and continue scheduling.
+      * This method exists to ensure conscious exception
+      * handling. It could be done inside `run` method,
+      * but it's often forgotten.
+      */
+    def onException(th: Throwable)
+    /**
+      * Delay between executions. On initial invocation,
+      * this is the delay after scheduling, on subsequent
+      * invocations, it's the delay after the previous run
+      * ended.
+      */
     def runDelay(): (Int, TimeUnit)
+    /**
+      * The jitter margin applied to the `runDelay`.
+      */
     def delayJitter: Float = 0.2f
-    def kill() = alive = false
+
+    /**
+      * Stop further scheduling.
+      */
+    def stop() = alive = false
     final def isAlive = alive
   }
 }
@@ -32,15 +56,12 @@ class FuzzyScheduler(scheduler: ScheduledExecutorService = Executors.newSchedule
     val delayMs = round(random * jitterRange + minDelay)
     val nextRun = new Runnable {
       def run = if (pr.isAlive) {
-        var scheduleAgain = true
         try {
           pr.run()
         } catch {
-          case e: Exception ⇒
-            e.printStackTrace()
-            scheduleAgain = false
+          case th: Throwable ⇒ pr.onException(th)
         }
-        if (scheduleAgain && pr.isAlive) try {
+        if (pr.isAlive) try {
           schedule(pr)
         } catch {
           case e: RejectedExecutionException ⇒ // Ok
