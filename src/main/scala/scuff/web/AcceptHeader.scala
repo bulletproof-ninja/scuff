96e23715cc1a570003c1a59985ebcb3842c77e70
nilskp <nilskp@gmail.com>
2013-06-06 09:40:47 -0500

    Refactored for better correctness

--- src/main/scala/scuff/web/AcceptHeader.scala
+++ src/main/scala/scuff/web/AcceptHeader.scala
@@ -1,30 +1,82 @@
 package scuff.web
 
-import javax.activation.{ MimeType, MimeTypeParameterList }
+import scala.util.Try
+import javax.activation.MimeType
 
 final class AcceptHeader(mimeTypes: Seq[MimeType]) {
-  def accepts(query: String): Boolean = accepts(new MimeType(query))
-  def accepts(query: MimeType): Boolean = mimeTypes.exists { target ⇒
-    query.getBaseType == target.getBaseType &&
-      {
-        val parms = query.getParameters
-        parms.isEmpty || matches(parms, target.getParameters)
-      }
+  require(!mimeTypes.isEmpty, "Cannot have an empty Accept header")
+  private[this] val hasMatchAny = mimeTypes.exists(mt ⇒ mt.getPrimaryType() == "*")
+  private def matchesTypes(specific: MimeType) = mimeTypes.exists { mt ⇒
+    mt.getPrimaryType == specific.getPrimaryType && (mt.getSubType == "*" || mt.getSubType == specific.getSubType)
   }
-  private def matches(query: MimeTypeParameterList, target: MimeTypeParameterList): Boolean = {
-    import collection.JavaConverters._
-    query.getNames.asScala.asInstanceOf[Iterator[String]].forall { qn ⇒
-      query.get(qn) == target.get(qn)
+  def preference(): MimeType = preferenceOrderd().head
+  def preferenceOrderd(): Seq[MimeType] = {
+    if (mimeTypes.size == 1) mimeTypes else {
+      val withQ = mimeTypes.zipWithIndex.map {
+        case (mt, idx) ⇒
+          val q = mt.getParameter("q") match {
+            case null ⇒ 1f
+            case q ⇒ Try(q.toFloat).getOrElse(0f)
+          }
+          (q, mt, idx)
+      }
+      withQ.sorted(AcceptHeader.MimeTypeOrdering).map(_._2)
     }
   }
-}
+  def matches(specific: String): Boolean = hasMatchAny || matchesTypes(new MimeType(specific))
+  def matches(specific: MimeType): Boolean = hasMatchAny || matchesTypes(specific)
+  def matchesAny(specifics: Traversable[MimeType]) = hasMatchAny || specifics.exists(matchesTypes)
+      }
 
 object AcceptHeader {
-  private val Splitter = ",".r.pattern
-  def apply(req: javax.servlet.http.HttpServletRequest): Option[AcceptHeader] = {
-    Option(req.getHeader("Accept")).map { header ⇒
-      val types = Splitter.split(header).map(new MimeType(_))
-      new AcceptHeader(types)
+  private type MTT = (Float, MimeType, Int)
+  private val MimeTypeOrdering = new math.Ordering[MTT] {
+    def compare(x: MTT, y: MTT): Int = {
+      if (x._1 == y._1) {
+        val xmt = x._2
+        val ymt = y._2
+        if (xmt.getPrimaryType == "*") {
+          1
+        } else if (ymt.getPrimaryType == "*") {
+          -1
+        } else if (xmt.getPrimaryType == ymt.getPrimaryType) {
+          if (xmt.getSubType == "*") {
+            1
+          } else if (ymt.getSubType == "*") {
+            -1
+          } else if (xmt.getSubType == ymt.getSubType) {
+            -xmt.toString.compareToIgnoreCase(ymt.toString)
+          } else {
+            x._3 - y._3
+          }
+        } else {
+          -xmt.toString.compareToIgnoreCase(ymt.toString)
+        }
+      } else if (x._1 > y._1) {
+        -1
+      } else {
+        1
+  }
     }
+  }
+  private val Splitter = """\s*,\s*""".r.pattern
+  private val EmptyArray = new Array[MimeType](0)
+  private def split(str: String): Seq[MimeType] = {
+    val types = Splitter.split(str.trim)
+    types.length match {
+      case 0 ⇒ Seq.empty
+      case 1 if types(0).length == 0 ⇒ EmptyArray
+      case _ ⇒ types.map(new MimeType(_))
+    }
+  }
+  def apply(header: String): Option[AcceptHeader] = Option(header).flatMap { header ⇒
+    val types = split(header)
+    if (types.isEmpty) None else Some(new AcceptHeader(types))
+}
+
+  def apply(req: javax.servlet.http.HttpServletRequest): Option[AcceptHeader] = {
+    import collection.JavaConverters._
+    val types = req.getHeaders("Accept").asScala.flatMap(split(_)).toSeq
+    if (types.isEmpty) None else Some(new AcceptHeader(types))
   }
 }
---
+++ src/test/scala/scuff/web/AcceptHeaderTest.scala
@@ -0,0 +1,71 @@
+package scuff.web
+
+import org.junit._
+import org.junit.Assert._
+import javax.activation.MimeType
+
+class AcceptHeaderTest {
+  @Test
+  def basic {
+    val acceptTypes = Set(new MimeType("text/html"))
+    assertTrue(AcceptHeader("text/html").get.matches("text/html"))
+    assertTrue(AcceptHeader("*/*").get.matches("text/html"))
+    assertTrue(AcceptHeader("text/*").get.matches("text/html"))
+    assertFalse(AcceptHeader("image/*").get.matches("text/html"))
+    assertFalse(AcceptHeader("text/*").get.matches("image/jpeg"))
+    assertTrue(AcceptHeader("*/*").get.matches("image/jpeg"))
+    assertTrue(AcceptHeader("*/*").forall(h ⇒ h.matchesAny(acceptTypes)))
+    assertTrue(AcceptHeader("text/*").forall(h ⇒ h.matchesAny(acceptTypes)))
+    assertFalse(AcceptHeader("image/*").forall(h ⇒ h.matchesAny(acceptTypes)))
+    assertTrue(AcceptHeader("").forall(h ⇒ h.matchesAny(acceptTypes)))
+  }
+
+  @Test
+  def complex {
+    val ah = AcceptHeader("text/html; q=1.0, text/*; q=0.8, image/gif; q=0.6, image/jpeg; q=0.6, image/*; q=0.5").get
+    assertTrue(ah.matches("image/png"))
+    assertTrue(ah.matches("text/plain"))
+    assertFalse(ah.matches("application/json"))
+  }
+
+  @Test
+  def preference {
+    val ah = AcceptHeader("text/html; q=1.0, text/*; q=0.8, image/gif; q=0.6, image/jpeg; q=0.6, image/*; q=0.5, */*; q=0.1").get
+    assertTrue(ah.preference.`match`("text/html"))
+  }
+  @Test
+  def rfc2616_1 {
+    val ordered = AcceptHeader("text/plain; q=0.5, text/html, text/x-dvi; q=0.8, text/x-c").get.preferenceOrderd
+    assertEquals(4, ordered.size)
+    assertTrue(ordered(0).`match`("text/html"))
+    assertTrue(ordered(1).`match`("text/x-c"))
+    assertTrue(ordered(2).`match`("text/x-dvi"))
+    assertTrue(ordered(3).`match`("text/plain"))
+  }
+  @Test
+  def rfc2616_2 {
+    val ordered = AcceptHeader("text/*, text/html, text/html;level=1, */*").get.preferenceOrderd
+    assertEquals(4, ordered.size)
+    assertEquals("text/html; level=1", ordered(0).toString)
+    assertEquals("text/html", ordered(1).toString)
+    assertEquals("text/*", ordered(2).toString)
+    assertEquals("*/*", ordered(3).toString)
+  }
+  @Test
+  def rfc2616_3 {
+    val ah = AcceptHeader("audio/*; q=0.2, audio/basic").get
+    assertEquals("audio/basic", ah.preference.toString)
+  }
+  @Test
+  def rfc2616_4 {
+    val ah = AcceptHeader("text/*;q=0.3, text/html;q=0.7, text/html;level=1, text/html;level=2;q=0.4, */*;q=0.5").get
+    val ordered = ah.preferenceOrderd
+    assertEquals(5, ordered.size)
+    assertEquals("text/html; level=1", ordered(0).toString)
+    assertTrue(ordered(1).`match`("text/html;level=3"))
+    assertTrue(ordered(1).`match`("text/html"))
+    assertEquals("*/*; q=0.5", ordered(2).toString)
+    assertTrue(ah.matches("image/jpeg"))
+    assertEquals(new MimeType("text/html; level=2; q=0.4").toString, ordered(3).toString)
+  }
+}
