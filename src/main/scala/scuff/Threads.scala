3753b585612f5ff1956e4ef4281785c9f4a13d02
nilskp <nilskp@gmail.com>
2013-07-15 11:25:04 -0500

    Rename

--- src/main/scala/scuff/Threads.scala
+++ src/main/scala/scuff/Threads.scala
@@ -1,24 +1,26 @@
 package scuff
 
-object ThreadFactory {
+import java.util.concurrent.ThreadFactory
+
+object Threads {
   val SystemThreadGroup = rootThreadGroup(Thread.currentThread.getThreadGroup)
   private def rootThreadGroup(group: ThreadGroup): ThreadGroup = {
     if (group.getParent == null) group
     else rootThreadGroup(group.getParent)
   }
-  private def newThreadGroup(name: String) = new ThreadGroup(ThreadFactory.SystemThreadGroup, name)
+  private def newThreadGroup(name: String) = new ThreadGroup(Threads.SystemThreadGroup, name)
 
-  def apply(name: String, threadGroup: ThreadGroup = null) = {
+  def factory(name: String, threadGroup: ThreadGroup = null): ThreadFactory = {
     val tg = if (threadGroup != null) threadGroup else newThreadGroup(name)
-    new ThreadFactory(name, tg, tg, false)
+    new ScuffThreadFactory(name, tg, tg, false)
   }
-  def daemon(name: String, threadGroup: ThreadGroup = null) = {
+  def daemonFactory(name: String, threadGroup: ThreadGroup = null): ThreadFactory = {
     val tg = if (threadGroup != null) threadGroup else newThreadGroup(name)
-    new ThreadFactory(name, tg, tg, true)
+    new ScuffThreadFactory(name, tg, tg, true)
   }
 }
 
-class ThreadFactory(name: String, threadGroup: ThreadGroup, exceptionHandler: Thread.UncaughtExceptionHandler, daemon: Boolean) extends java.util.concurrent.ThreadFactory {
+private[scuff] class ScuffThreadFactory(name: String, threadGroup: ThreadGroup, exceptionHandler: Thread.UncaughtExceptionHandler, daemon: Boolean) extends ThreadFactory {
   private val nameFormat = name + "[%d]"
   private val counter = new java.util.concurrent.atomic.AtomicInteger
   private def newName = nameFormat.format(counter.getAndIncrement)
