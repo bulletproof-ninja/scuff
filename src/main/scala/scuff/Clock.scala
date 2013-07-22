505be6a8fec6e9e3e6d630fafdb4c74537693ea6
nilskp <nilskp@gmail.com>
2013-07-22 12:30:45 -0500

    Add convenience timestamp method

--- src/main/scala/scuff/Clock.scala
+++ src/main/scala/scuff/Clock.scala
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit
 trait Clock {
   def precision: TimeUnit
   def now(implicit toPrecision: TimeUnit): Long
+  @inline final def timestamp = new Timestamp(now(TimeUnit.MILLISECONDS))
   @inline final def durationSince(earlier: Long)(implicit toPrecision: TimeUnit): Long = now - earlier
   @inline final def durationUntil(future: Long)(implicit toPrecision: TimeUnit): Long = future - now
 }
