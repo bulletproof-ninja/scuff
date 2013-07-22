46c92c9c21cbfb1bf90f6ca11363a283176dea1c
nilskp <nilskp@gmail.com>
2013-07-22 12:33:49 -0500

    Use Try instead of Either, and add an Value class sandwich.

--- src/main/scala/scuff/Proxylicious.scala
+++ src/main/scala/scuff/Proxylicious.scala
@@ -2,13 +2,14 @@ package scuff
 
 import java.lang.reflect.{ InvocationHandler, Proxy, Method, UndeclaredThrowableException, InvocationTargetException }
 import reflect.ClassTag
+import scala.util.{ Try, Success, Failure }
 
 /**
  * Interface proxy factory.
  */
-class Proxylicious[T](implicit manifest: ClassTag[T]) {
+class Proxylicious[T](implicit tag: ClassTag[T]) {
 
-  private def newProxy(ih: InvocationHandler) = Proxy.newProxyInstance(manifest.runtimeClass.getClassLoader, Array(manifest.runtimeClass), ih).asInstanceOf[T]
+  private def newProxy(ih: InvocationHandler) = Proxy.newProxyInstance(tag.runtimeClass.getClassLoader, Array(tag.runtimeClass), ih).asInstanceOf[T]
 
   def proxify(handler: (T, Method, Array[AnyRef]) ⇒ Any): T = {
     this newProxy new InvocationHandler {
@@ -23,18 +24,18 @@ class Proxylicious[T](implicit manifest: ClassTag[T]) {
         val anyArgs = args.asInstanceOf[Array[Any]]
         val include = wrapper.include(method)
         if (include) wrapper.before(proxy, method, anyArgs)
-        val result: Either[Throwable, Any] = try {
-          Right(method.invoke(meat, args: _*).asInstanceOf[Any])
+        val result: Try[AnyRef] = try {
+          Success(method.invoke(meat, args: _*))
         } catch {
-          case t: InvocationTargetException ⇒ Left(t.getTargetException)
-          case t: UndeclaredThrowableException ⇒ Left(t.getUndeclaredThrowable)
-          case t: Exception ⇒ Left(t)
+          case t: InvocationTargetException ⇒ Failure(t.getTargetException)
+          case t: UndeclaredThrowableException ⇒ Failure(t.getUndeclaredThrowable)
+          case t: Throwable ⇒ Failure(t)
         }
         if (include) {
           wrapper.after(proxy, method, anyArgs, result).asInstanceOf[Object]
         } else result match {
-          case Left(t) ⇒ throw t
-          case Right(r) ⇒ r.asInstanceOf[AnyRef]
+          case Failure(t) ⇒ throw t
+          case Success(res) ⇒ res
         }
       }
     }
@@ -46,7 +47,7 @@ class Proxylicious[T](implicit manifest: ClassTag[T]) {
     }
     class DSLString(method: Method, methodName: String = null) {
       def returns(returnType: Class[_]): Boolean = (methodName == null || nameMatches) && method.getReturnType() == returnType
-      implicit def nameMatches() = method.getName() == methodName
+      def nameMatches() = method.getName() == methodName
     }
     implicit def method2dslm(method: Method) = new DSLMethod(method)
     implicit def method2dsls(method: Method) = new DSLString(method)
@@ -63,8 +64,44 @@ class Proxylicious[T](implicit manifest: ClassTag[T]) {
     /**
      * Called after invoking method.
      */
-    def after(proxy: T, method: Method, args: Array[Any], result: Either[Throwable, Any]): Any
+    def after(proxy: T, method: Method, args: Array[Any], result: Try[Any]): Any
+  }
+
+  private def isSingleObjectArg(args: Array[Class[_]]) = args.length == 1 && args(0) == classOf[Object]
+
+  /**
+   * Override equals/hashCode like a value class, i.e.
+   * equality is based on all accessible values, not
+   * identity. Same with hash code.
+   */
+  def EqualsHashCodeOverride: Sandwich = new Sandwich {
+    def include(method: Method) =
+      (isSingleObjectArg(method.getParameterTypes) && method.getName == "equals" && method.getReturnType == classOf[Boolean]) ||
+        (method.getParameterTypes.length == 0 && method.getName == "hashCode" && method.getReturnType == classOf[Int])
+    def before(proxy: T, method: Method, args: Array[Any]) {}
+    def after(proxy: T, method: Method, args: Array[Any], result: Try[Any]): Any = {
+      val getters = tag.runtimeClass.getMethods().filter(_.getParameterTypes.length == 0)
+      method.getName match {
+        case "equals" ⇒ allEqual(proxy, args(0), getters)
+        case "hashCode" ⇒ calcHash(proxy, getters)
+        case _ ⇒ result match {
+          case Success(res) ⇒ res
+          case Failure(t) ⇒ throw t
+        }
+      }
+    }
+    private def allEqual(t1: T, t2: Any, getters: Seq[Method]): Boolean = getters.forall { m ⇒
+      val v1 = m.invoke(t1)
+      val v2 = m.invoke(t2)
+      v1 == v2
+    }
+    private def calcHash(t: T, getters: Seq[Method]): Int = getters.foldLeft(17) { (hash, m) ⇒
+      m.invoke(t) match {
+        case null ⇒ 31 * hash
+        case value ⇒ 31 * hash + value.hashCode
+      }
+    }
+
   }
 
 }
-
