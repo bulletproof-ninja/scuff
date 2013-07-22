88167c2eebcf26ec547a65c09cb2156c9a7325f5
nilskp <nilskp@gmail.com>
2013-07-22 12:47:48 -0500

    Try replaces Either

--- src/main/scala/scuff/util/RetryOnExceptionProxylicious.scala
+++ src/main/scala/scuff/util/RetryOnExceptionProxylicious.scala
@@ -2,7 +2,7 @@ package scuff.util
 
 import scuff.Proxylicious
 import java.lang.reflect.Method
-
+import scala.util._
 /**
  * Retry all method calls on certain exception.
  */
@@ -10,9 +10,9 @@ class RetryOnExceptionProxylicious[T, E <: Throwable](implicit manifest: reflect
   class RetryingSandwich extends Sandwich {
     def include(method: Method) = true
     def before(proxy: T, method: Method, args: Array[Any]) = ()
-    def after(proxy: T, method: Method, args: Array[Any], result: Either[Throwable, Any]): Any = result match {
-      case Right(r) ⇒ r
-      case Left(e) ⇒
+    def after(proxy: T, method: Method, args: Array[Any], result: Try[Any]): Any = result match {
+      case Success(r) ⇒ r
+      case Failure(e) ⇒
         if (retryException.runtimeClass.isInstance(e)) {
           method.invoke(proxy, args.asInstanceOf[Array[Object]]: _*)
         } else {
