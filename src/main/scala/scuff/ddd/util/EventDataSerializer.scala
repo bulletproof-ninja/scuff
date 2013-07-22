0f5094ac0f2ad74682dd696a5f7ba35381581e4d
nilskp <nilskp@gmail.com>
2013-07-22 12:35:03 -0500

    Remove

--- src/main/scala/scuff/ddd/util/EventDataSerializer.scala
+++
@@ -1,8 +0,0 @@
-package scuff.ddd.util
-
-import scuff.ddd._
-
-trait EventDataSerializer[EVT <: DomainEvent, S] {
-  def snapshotData(evt: EVT): S
-  def rebuildEvent(version: Short, data: S): EVT
-}
