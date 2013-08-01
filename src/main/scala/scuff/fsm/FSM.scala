58e3f551db22905b71b629b7c49e4c143aecf9b7
nilskp <nilskp@gmail.com>
2013-07-31 21:25:13 -0500

    Big refactor. Allows conditional transitions AND simplified code (onEvent replaces onEntry and onExit)

--- src/main/scala/scuff/fsm/FSM.scala
+++ src/main/scala/scuff/fsm/FSM.scala
@@ -1,105 +1,105 @@
 package scuff.fsm
 
 object typed {
-  sealed trait Source[-T] extends State {
-  protected[fsm] def onExit(evt: Event) {}
-    protected[fsm] def onExit(evt: Event, payload: T): Unit = onExit(evt)
-}
-  sealed trait Target[-T] extends State {
-  protected[fsm] def onEntry(evt: Event) {}
-    protected[fsm] def onEntry(evt: Event, payload: T): Unit = onEntry(evt)
-}
+  sealed trait Source[-T] extends BaseState[T]
+  sealed trait Target[-T] extends BaseState[T]
   class SuperState[T](name: String = "") extends Source[T] {
-  override def toString = if (name.length == 0) super.toString else name
-}
-  class LeafState[T](name: String = "") extends Source[T] with Target[T] {
-  override def toString = if (name.length == 0) super.toString else name
-}
+    override def toString = if (name.length == 0) super.toString else name
+  }
+  class State[T](name: String = "") extends Source[T] with Target[T] {
+    override def toString = if (name.length == 0) super.toString else name
+  }
   class FinalState[T](name: String = "") extends Target[T] {
-  override def toString = if (name.length == 0) super.toString else name
-}
+    override def toString = if (name.length == 0) super.toString else name
+  }
   trait FSM[T] extends typed.SuperState[T] {
 
-    private[this] def assignParenthood(superState: typed.SuperState[T]): Unit = {
-    val parent = Some(superState)
-    superState.getClass.getDeclaredFields.withFilter(f ⇒ classOf[State].isAssignableFrom(f.getType)).foreach { field ⇒
-      field.setAccessible(true)
-      val subState = field.get(superState)
-      new scuff.Surgeon(subState).set('parent, parent)
-      subState match {
-          case ss: typed.SuperState[T] ⇒ assignParenthood(ss)
-        case _ ⇒ // Ignore
+    private[this] def assignParenthood(superState: typed.SuperState[T], ignore: collection.mutable.Set[Any]): Unit = {
+      val parent = Some(superState)
+      val baseStates = superState.getClass.getDeclaredFields.filter(f ⇒ classOf[BaseState[T]].isAssignableFrom(f.getType))
+      baseStates.foreach { field ⇒
+        field.setAccessible(true)
+        val subState = field.get(superState)
+        if (!ignore.contains(subState)) {
+          new scuff.Surgeon(subState).set('parent, parent).set('assignedName, field.getName)
+          ignore += subState
+          subState match {
+            case ss: typed.SuperState[T] ⇒ assignParenthood(ss, ignore)
+            case _ ⇒ // Ignore
+          }
+        }
       }
     }
-  }
 
-  @annotation.tailrec
-  private def stateMatch(checkState: State, current: Option[State]): Boolean = current match {
-    case None ⇒ false
-    case Some(current) ⇒
-      if (current eq checkState) {
-        true
-      } else {
-        stateMatch(checkState, current.parent)
-      }
-  }
+    @annotation.tailrec
+    private def stateMatch(checkState: BaseState[T], current: Option[BaseState[T]]): Boolean = current match {
+      case None ⇒ false
+      case Some(current) ⇒ (current eq checkState) || stateMatch(checkState, current.parent)
+    }
 
-  def is(state: State) = stateMatch(state, currState)
-  def current = currState
-  def isFinal() = currState match {
+    def is(state: BaseState[T]) = stateMatch(state, currState)
+    def current = currState
+    def isFinal() = currState match {
       case Some(_: FinalState[_]) ⇒ true
-    case _ ⇒ false
-  }
+      case _ ⇒ false
+    }
 
     type Transition = ((typed.Source[T], Event), typed.Target[T])
-  protected def transitions: Set[Transition]
+    protected def transitions: Set[Transition]
     private var currState: Option[typed.Target[T]] = None
     private var transitionTable: Map[(typed.Source[T], Event), typed.Target[T]] = _
-    protected def start(initialState: typed.LeafState[T]) = {
-    transitionTable = transitions.toMap
-    assignParenthood(this)
-    currState = Option(initialState)
-  }
+    protected def start(initialState: typed.State[T]) = {
+      transitionTable = transitions.toMap
+      require(transitionTable.size == transitions.size, "Duplicate State -> Event transition")
+      assignParenthood(this, collection.mutable.Set(this))
+      currState = Option(initialState)
+    }
     def apply(evt: Event, payload: T = null.asInstanceOf[T]) = currState match {
-    case None ⇒ throw new IllegalStateException("State machine not started yet")
+      case None ⇒ throw new IllegalStateException("State machine not started yet")
       case Some(source: typed.Source[T]) ⇒
-      val target = transition(source, evt)
-      source.onExit(evt, payload)
-      currState = Some(target)
-      target.onEntry(evt, payload)
-    case _ ⇒ throw new IllegalStateException("State machine is finalized")
-  }
-  @annotation.tailrec
+        val targetEvent = source.onEvent(evt, payload)
+        val target = transition(source, targetEvent)
+        if (source ne target) {
+          currState = Some(target)
+          target.onEvent(targetEvent, payload)
+        }
+      case _ ⇒ throw new IllegalStateException("State machine is finalized")
+    }
+    @annotation.tailrec
     private def transition(state: typed.Source[T], evt: Event): typed.Target[T] = {
-    val key = state -> evt
-    transitionTable.get(key) match {
-      case Some(toState) ⇒ toState
-      case None ⇒ state.parent match {
-        case None ⇒ throw new IllegalStateException("%s cannot handle %s".format(currState.get, evt))
-        case Some(parent) ⇒ transition(parent, evt)
+      val key = state -> evt
+      transitionTable.get(key) match {
+        case Some(toState) ⇒ toState
+        case None ⇒ state.parent match {
+          case None ⇒ throw new IllegalStateException("%s cannot handle %s".format(currState.get, evt))
+          case Some(parent) ⇒ transition(parent, evt)
+        }
       }
     }
   }
-}
 }
 /** Event marker trait. */
 trait Event
 
 /** General state representation. */
-sealed class State {
+private[fsm] sealed class BaseState[-T] {
   private[fsm] final val parent: Option[typed.SuperState[Any]] = None
+  private[this] val assignedName: String = getClass.getSimpleName
+  override def toString: String = parent.map(_.toString concat ".").getOrElse("") concat assignedName
+  protected[fsm] def onEvent(evt: Event) {}
+  protected[fsm] def onEvent(evt: Event, payload: T): Event = { onEvent(evt); evt }
 }
 
 /** Super state. Is expected to contain other states as `val`s. */
 class SuperState(name: String = "") extends typed.SuperState[Any]
 
 /** Leaf state. Is expected to not contain other sub states. */
-class LeafState(name: String = "") extends typed.LeafState[Any]
+class State(name: String = "") extends typed.State[Any]
 
 /** Final state. */
 class FinalState(name: String = "") extends typed.FinalState[Any]
 
 /**
-  * A finite state machine trait, which is itself a super state.
-  */
+ * A finite state machine trait, which is itself a super state.
+ */
 trait FSM extends typed.FSM[Any]
--- src/test/scala/scuff/fsm/TestMachine.scala
+++ src/test/scala/scuff/fsm/TestMachine.scala
@@ -2,12 +2,13 @@ package scuff.fsm
 
 import org.junit._
 import org.junit.Assert._
-
 import language.reflectiveCalls
+import scala.util.Try
 
 class TestFSM {
   @Test
   def connection {
+    import ConnectionEvents._
     val conn = new Connection
     import conn.{ Active, Disabled, Forgotten }
     assertFalse(conn.isFinal)
@@ -50,84 +51,205 @@ class TestFSM {
     }
   }
 
-case object enable extends Event
-case object disable extends Event
-case object reconnected extends Event
-case object disconnected extends Event
-case object forget extends Event
-case object silence extends Event
-
-class Connection extends FSM {
-
-  def start(enabled: Boolean) = {
-    val initial = if (enabled) Active.Connected else Disabled.Connected
-    super.start(initial)
+  object ConnectionEvents extends Enumeration {
+    val enable, disable, reconnected, disconnected, forget, silence = new Val() with Event
   }
 
-  val Active = new SuperState {
-    val Connected = new LeafState
-    val Disconnected = new SuperState {
-      val Alarm = new LeafState
-      val Silent = new LeafState
+  class Connection extends FSM {
+
+    def start(enabled: Boolean) = {
+      val initial = if (enabled) Active.Connected else Disabled.Connected
+      super.start(initial)
     }
+
+    val Active = new SuperState {
+      val Connected = new State
+      val Disconnected = new SuperState {
+        val Alarm = new State
+        val Silent = new State
+      }
+    }
+    val Disabled = new SuperState {
+      val Connected = new State
+      val Disconnected = new State
+    }
+    val Forgotten = new FinalState
+
+    import ConnectionEvents._
+    val transitions: Set[Transition] = Set(
+      Active.Connected -> disable -> Disabled.Connected,
+      Active.Connected -> disconnected -> Active.Disconnected.Alarm,
+      Active.Disconnected.Alarm -> silence -> Active.Disconnected.Silent,
+      Active.Disconnected -> reconnected -> Active.Connected,
+      Active.Disconnected -> forget -> Forgotten,
+
+      Disabled.Connected -> disconnected -> Disabled.Disconnected,
+      Disabled.Disconnected -> reconnected -> Disabled.Connected,
+      Disabled.Disconnected -> forget -> Forgotten)
+
+    def isDisconnected = is(Disabled.Disconnected) || is(Active.Disconnected)
   }
-  val Disabled = new SuperState {
-    val Connected = new LeafState
-    val Disconnected = new LeafState
-  }
-  val Forgotten = new FinalState
-
-  val transitions: Set[Transition] = Set(
-    Active.Connected -> disable -> Disabled.Connected,
-    Active.Connected -> disconnected -> Active.Disconnected.Alarm,
-    Active.Disconnected.Alarm -> silence -> Active.Disconnected.Silent,
-    Active.Disconnected -> reconnected -> Active.Connected,
-    Active.Disconnected -> forget -> Forgotten,
-
-    Disabled.Connected -> disconnected -> Disabled.Disconnected,
-    Disabled.Disconnected -> reconnected -> Disabled.Connected,
-    Disabled.Disconnected -> forget -> Forgotten
-  )
-
-  def isDisconnected = is(Disabled.Disconnected) || is(Active.Disconnected)
-}
 
   class ISODateParser extends typed.FSM[Char] {
 
-  def start() {
-    super.start(ParseYear)
-  }
-
-  object digit extends Event
-  object dash extends Event
-  object done extends Event
-
-    sealed class ParseState extends typed.LeafState[Char] {
-    var num = 0
-      override def onEntry(evt: Event, char: Char) = evt match {
-        case `digit` ⇒ onDigit(char)
-        case `dash` ⇒ // Ignore
-      }
-    def onDigit(char: Char) = num = (num * 10) + (char - '0')
-  }
-  val ParseYear = new ParseState
-  val ParseMonth = new ParseState
-  val ParseDay = new ParseState
-  val Completed = new FinalState {
-    var date: Option[java.sql.Date] = None
-    override def onEntry(evt: Event) = evt match {
-      case `done` ⇒ date = Some(new java.sql.Date(ParseYear.num - 1900, ParseMonth.num - 1, ParseDay.num))
+    def start() {
+      super.start(ParseYear)
     }
+
+    object digit extends Event
+    object dash extends Event
+    object done extends Event
+
+    sealed class ParseState extends typed.State[Char] {
+      var num = 0
+      override def onEvent(evt: Event, char: Char) = evt match {
+        case `digit` ⇒
+          onDigit(char)
+          evt
+        case _ ⇒ evt
+      }
+      def onDigit(char: Char) = num = (num * 10) + (char - '0')
+    }
+    val ParseYear = new ParseState
+    val ParseMonth = new ParseState
+    val ParseDay = new ParseState
+    val Completed = new FinalState {
+      var date: Option[java.sql.Date] = None
+      override def onEvent(evt: Event) = evt match {
+        case `done` ⇒ date = Some(new java.sql.Date(ParseYear.num - 1900, ParseMonth.num - 1, ParseDay.num))
+      }
+    }
+
+    val transitions: Set[Transition] = Set(
+      ParseYear -> digit -> ParseYear,
+      ParseYear -> dash -> ParseMonth,
+      ParseMonth -> digit -> ParseMonth,
+      ParseMonth -> dash -> ParseDay,
+      ParseDay -> digit -> ParseDay,
+      ParseDay -> done -> Completed)
+  }
+  @Test
+  def `phone, invalid number` {
+    import OldSchoolPhoneEvents._
+    val phone = new OldSchoolPhone
+    import phone._
+    phone.plugIn()
+    assertTrue(phone is Idle)
+    assertTrue(Try(phone(TimedOut)).isFailure)
+    phone(ReceiverLifted)
+    assertTrue(phone is Active)
+    phone(HungUp)
+    assertFalse(phone is Active)
+    assertTrue(phone is Idle)
+    phone(DigitDialed, '0')
+    assertTrue(phone is Idle)
+    phone(ReceiverLifted)
+    phone(DigitDialed, '0')
+    assertTrue(phone is Active.InvalidNumber)
+  }
+  @Test
+  def `phone, busy, try again, talk` {
+    val number = "234-235-5678".filterNot(_ == '-')
+    import OldSchoolPhoneEvents._
+    val phone = new OldSchoolPhone
+    import phone._
+    phone.plugIn()
+    phone(ReceiverLifted)
+    assertTrue(phone is Active.DialTone)
+    val (first, rest) = number.head -> number.tail
+    phone(DigitDialed, first)
+    rest.foreach { digit ⇒
+      assertTrue(phone is Active.Dialing)
+      phone(DigitDialed, digit)
+    }
+    assertTrue(phone is Active.Connecting)
+    phone(NumberBusy)
+    assertTrue(phone is Active.Busy)
+    phone(HungUp)
+    assertTrue(phone is Idle)
+
+    phone(ReceiverLifted)
+    assertTrue(phone is Active.DialTone)
+    number.foreach(phone(DigitDialed, _))
+    assertTrue(phone is Active.Connecting)
+    phone(Connected)
+    assertTrue(phone is Active.Ringing)
+    phone(HungUp)
+    assertTrue(phone is Idle)
+
+    phone(ReceiverLifted)
+    number.foreach(phone(DigitDialed, _))
+    assertEquals(number, Active.number.result)
+    phone(Connected)
+    assertTrue(phone is Active.Ringing)
+    phone(CalleeReceiverLifted)
+    assertTrue(phone is Active.Talking)
   }
 
-  val transitions: Set[Transition] = Set(
-    ParseYear -> digit -> ParseYear,
-    ParseYear -> dash -> ParseMonth,
-    ParseMonth -> digit -> ParseMonth,
-    ParseMonth -> dash -> ParseDay,
-    ParseDay -> digit -> ParseDay,
-    ParseDay -> done -> Completed
-  )
 }
-
+object OldSchoolPhoneEvents {
+  case object HungUp extends Event
+  case object ReceiverLifted extends Event
+  case object TimedOut extends Event
+  case object NumberBusy extends Event
+  case object Connected extends Event
+  case object CalleeReceiverLifted extends Event
+  case object CalleeHungUp extends Event
+  case object DigitDialed extends Event
+  case class DigitDialed(valid: Boolean = true, complete: Boolean = false) extends Event
+}
+import OldSchoolPhoneEvents._
+class OldSchoolPhone extends FSM {
+  private def toInt(char: Char) = char - '0'
+  def plugIn() = super.start(Idle)
+  val Idle = new State
+  val Active = new SuperState {
+    val number = new StringBuilder
+    val DialTone = new State {
+      override def onEvent(evt: Event, payload: Any): Event = evt match {
+        case DigitDialed ⇒
+          val digit = toInt(payload.asInstanceOf[Char])
+          DigitDialed(valid = (2 <= digit && digit <= 9))
+        case ReceiverLifted ⇒
+          number.clear()
+          evt
+        case _ ⇒ evt
       }
+    }
+    val Dialing = new State {
+      override def onEvent(evt: Event, payload: Any): Event = evt match {
+        case DigitDialed ⇒
+          val digit = toInt(payload.asInstanceOf[Char])
+          onEvent(DigitDialed(0 <= digit && digit <= 9), payload)
+        case DigitDialed(true, _) ⇒
+          number += payload.asInstanceOf[Char]
+          DigitDialed(true, number.length == 10)
+      }
+    }
+    val Connecting, Timeout, Busy, Ringing, Talking, Pinned, InvalidNumber = new State
+  }
+
+  import OldSchoolPhoneEvents._
+  import Active._
+  val transitions: Set[Transition] = Set(
+    Idle -> DigitDialed -> Idle,
+    Idle -> ReceiverLifted -> DialTone,
+    Active -> HungUp -> Idle,
+
+    DialTone -> DigitDialed(valid = true) -> Dialing,
+    DialTone -> TimedOut -> Timeout,
+    DialTone -> DigitDialed(valid = false) -> InvalidNumber,
+
+    Dialing -> DigitDialed(valid = true) -> Dialing,
+    Dialing -> TimedOut -> Timeout,
+    Dialing -> DigitDialed(valid = false) -> InvalidNumber,
+    Dialing -> DigitDialed(complete = true) -> Connecting,
+
+    Connecting -> NumberBusy -> Busy,
+    Connecting -> Connected -> Ringing,
+
+    Ringing -> CalleeReceiverLifted -> Talking,
+
+    Talking -> CalleeHungUp -> Pinned,
+    Pinned -> CalleeReceiverLifted -> Talking)
+}
