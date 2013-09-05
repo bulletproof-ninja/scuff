2f080a5bfc4aa3957096021fab88a63d97e4bf74
nilskp <nilskp@gmail.com>
2013-09-04 20:19:16 -0500

    reformat toString

--- src/main/scala/scuff/UserPrincipal.scala
+++ src/main/scala/scuff/UserPrincipal.scala
@@ -4,7 +4,7 @@ import java.security.Principal
 
 case class UserPrincipal(login: String, roles: Set[String]) extends Principal {
   def getName = login
-  override def toString = "%s:%s".format(login, roles)
+  override def toString = "%s[%s]".format(login, roles.mkString(","))
   override def equals(any: Any) = any match {
     case that: Principal ⇒ this.getName == that.getName
     case _ ⇒ false
