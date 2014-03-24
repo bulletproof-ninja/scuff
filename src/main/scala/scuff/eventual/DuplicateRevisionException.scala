0b8088e4752b3580ed580fc83acb0e676dd2da83
nilskp <nilskp@gmail.com>
2014-03-24 16:29:04 -0500

    make revision accessible

--- src/main/scala/scuff/eventual/DuplicateRevisionException.scala
+++ src/main/scala/scuff/eventual/DuplicateRevisionException.scala
@@ -6,6 +6,6 @@ import scala.util.control.NoStackTrace
  * There has been a concurrent revision update between
  * loading and updating the stream.
  */
-class DuplicateRevisionException(id: Any, revision: Int)
+class DuplicateRevisionException(id: Any, val revision: Int)
   extends RuntimeException("Revision %d already exists: %s".format(revision, id))
   with NoStackTrace
