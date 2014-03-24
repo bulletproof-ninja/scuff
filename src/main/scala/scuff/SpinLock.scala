7906fdac0c2d6c3f74f008ba42b3821bdadcf5c9
nilskp <nilskp@gmail.com>
2014-03-24 16:24:58 -0500

    clarify usage.

--- src/main/scala/scuff/SpinLock.scala
+++ src/main/scala/scuff/SpinLock.scala
@@ -2,7 +2,9 @@ package scuff
 
 /**
  * Reentrant spin lock. Useful for code that wants to avoid
- * context switching on contention.
+ * context switching on rare or no contention.
+ * NOTICE: This should NOT be used on highly (or even moderately)
+ * contended resources.
  */
 final class SpinLock {
   private[this] val lock = new java.util.concurrent.atomic.AtomicReference[Thread]
