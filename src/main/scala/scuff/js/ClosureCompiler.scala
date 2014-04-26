554c2feed25c7fb5439b4dffec7ef2f8441bf288
nilskp <nilskp@gmail.com>
2014-04-26 17:11:42 -0500

    go with basic simple optimizations

--- src/main/scala/scuff/js/ClosureCompiler.scala
+++ src/main/scala/scuff/js/ClosureCompiler.scala
@@ -9,32 +9,12 @@ object ClosureCompiler {
 
   final val Encoding = "UTF-8"
 
-  private val DefaultOptions = defaultOptions
-  def defaultOptions = {
-    val opts = new CompilerOptions
-    opts.aggressiveVarCheck = CheckLevel.ERROR
-    opts.checkSuspiciousCode = true
-    opts.foldConstants = true
-    opts.checkTypes = true
-    opts.reportMissingOverride = CheckLevel.ERROR
-    opts.optimizeReturns = true
-    opts.variableRenaming = VariableRenamingPolicy.LOCAL
-    opts.markNoSideEffectCalls = true
-    opts.propertyRenaming = PropertyRenamingPolicy.OFF
-    opts.inlineConstantVars = true
-    opts.inlineFunctions = true
-    opts.inlineLocalFunctions = true
-    opts.aliasExternals = true
-    opts.collapseProperties = true
-    opts.devirtualizePrototypeMethods = true
-    opts.moveFunctionDeclarations = true
-    opts.aliasAllStrings = true
-    opts.deadAssignmentElimination = true
-    opts.setSmartNameRemoval(true)
-    opts.setLanguageIn(CompilerOptions.LanguageMode.ECMASCRIPT5_STRICT)
-    opts.setLanguageOut(CompilerOptions.LanguageMode.ECMASCRIPT5_STRICT)
-    opts.setOutputCharset(Encoding)
-    opts
+  def DefaultOptions = {
+    val options = new CompilerOptions
+    CompilationLevel.SIMPLE_OPTIMIZATIONS.setOptionsForCompilationLevel(options)
+    options.setLanguageOut(CompilerOptions.LanguageMode.ECMASCRIPT5_STRICT)
+    options.setOutputCharset(Encoding)
+    options
   }
 
   def compile(code: String, fileName: String = "N/A", options: CompilerOptions = DefaultOptions): String = {
