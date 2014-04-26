3e77c8404c3f081b893533a3d1edb34411757a11
nilskp <nils@fdbk.com>
2014-04-26 11:22:48 -0500

    refactor

--- src/main/scala/scuff/eventual/ProcessingException.scala
+++ src/main/scala/scuff/eventual/ProcessingException.scala
@@ -6,12 +6,12 @@ class ProcessingException private[eventual] (message: String, cause: Throwable =
 
 object ProcessingException {
   def apply[A, B, C](cause: Throwable, txn: EventSource[A, B, C]#Transaction): ProcessingException = {
-    val msg = s"Failed processing ${txn.category} stream ${txn.streamId}/${txn.revision}: ${cause.getMessage}${compat.Platform.EOL}Metadata: ${txn.metadata.mkString(" | ")}"
+    val msg = s"Failed processing ${txn.category} ${txn.streamId} rev ${txn.revision}: ${cause.getMessage}${compat.Platform.EOL}Metadata: ${txn.metadata.mkString(" | ")}"
     new ProcessingException(msg, cause)
   }
 
   def outOfSequence[A, B, C](txn: EventSource[A, B, C]#Transaction, expectedRevision: Int): OutOfSequenceException = {
-    val msg = s"Failed processing ${txn.category} stream ${txn.streamId}/${txn.revision}: Expected revision $expectedRevision"
+    val msg = s"Failed processing ${txn.category} ${txn.streamId} rev ${txn.revision}: Expected revision $expectedRevision"
     new OutOfSequenceException(msg, expectedRevision)
   }
 }
