3411b2bd3216d98565821d9c0f39c2c29912ed32
nilskp <nilskp@gmail.com>
2014-05-06 17:33:21 -0500

    Add conversion of Java Future to Scala Future, using a single polling thread.

--- src/main/scala/scuff/Threads.scala
+++ src/main/scala/scuff/Threads.scala
@@ -4,11 +4,55 @@ import java.util.concurrent.ThreadFactory
 import java.util.concurrent.Executors
 import scala.concurrent.ExecutionContextExecutor
 import java.util.concurrent.Executor
+import scala.concurrent.Promise
+import scala.util.Try
 
 /**
  * Thread helper class.
  */
 object Threads {
+
+  private[this] lazy val _javaFutureConverter = {
+    val jfg = new JavaFutureConverter[Any]
+    jfg.thread.start()
+    jfg
+  }
+  def javaFutureConverter[T] = _javaFutureConverter.asInstanceOf[JavaFutureConverter[T]]
+  class JavaFutureConverter[T](sleepMs: Long = 1, failureReporter: Throwable => Unit = (t) => t.printStackTrace)
+      extends (java.util.concurrent.Future[T] => concurrent.Future[T]) {
+    type QueueItem[T] = (Promise[T], java.util.concurrent.Future[T])
+    private[this] val queue = new collection.mutable.Queue[QueueItem[T]]
+    private[Threads] val thread = new Thread("scuff.Threads.JavaFutureGetter") {
+      this setUncaughtExceptionHandler new Thread.UncaughtExceptionHandler {
+        def uncaughtException(t: Thread, e: Throwable) {
+          failureReporter(e)
+        }
+      }
+      override def run {
+        while (!Thread.currentThread.isInterrupted) {
+          queue.synchronized {
+            if (queue.isEmpty) {
+              queue.wait()
+            } else {
+              queue.dequeueAll(_._2.isDone()).foreach {
+                case (promise, f) => promise complete Try(f.get)
+              }
+              Thread sleep sleepMs
+            }
+          }
+        }
+      }
+    }
+    def apply(f: java.util.concurrent.Future[T]): concurrent.Future[T] = {
+      val promise = Promise[T]
+      queue.synchronized {
+        val notify = queue.isEmpty
+        queue enqueue promise -> f
+        queue.notify()
+      }
+      promise.future
+    }
+  }
 
   lazy val DefaultScheduler = Executors.newScheduledThreadPool(Runtime.getRuntime.availableProcessors, Threads.daemonFactory("scuff.DefaultScheduler"))
 
@@ -18,9 +62,12 @@ object Threads {
       r.run()
     }
   }
-  
+
   /**
    * `ExecutionContext`, which executes on the same thread.
+   * Use this to prevent a cheap execution having to pass through
+   * another to another thread, and only use this when you're certain
+   * that it's OK to execute on the existing thread.
    */
   abstract class SameThreadExecutor extends concurrent.ExecutionContextExecutor {
     def execute(runnable: Runnable) = runnable.run()
--- src/test/scala/scuff/TestThreads.scala
+++ src/test/scala/scuff/TestThreads.scala
@@ -3,6 +3,10 @@ package scuff
 import org.junit._
 import org.junit.Assert._
 import java.util.concurrent._
+import scala.util.Random
+import scala.util.Failure
+import scala.util.Success
+import concurrent.blocking
 
 class TestThreads extends Serializable {
   @Test
@@ -16,6 +20,41 @@ class TestThreads extends Serializable {
     assertEquals("MyThread", thread.getThreadGroup.getName)
     thread.start()
     assertTrue(latch.await(2, TimeUnit.SECONDS))
+  }
+
+  @Test
+  def javaFutures {
+    implicit val ec = concurrent.ExecutionContext.global
+    val rand = new Random
+    val futures = (1 to 1500).map { i =>
+      val f = new java.util.concurrent.Future[Int] {
+        val queue = new LinkedBlockingQueue[Int](1)
+        def cancel(now: Boolean) = ???
+        def isCancelled() = false
+        def isDone(): Boolean = !queue.isEmpty()
+        def get() = queue.remove()
+        def get(t: Long, tu: TimeUnit): Int = ???
+      }
+      ec execute new Runnable {
+        override def run = {
+          Thread sleep rand.nextInRange(1 to 5)
+          f.queue.put(i)
+        }
+      }
+      f.asScala
+    }
+    val set = new collection.concurrent.TrieMap[Int, Unit]
+    val cdl = new CountDownLatch(futures.size)
+    futures.foreach { f =>
+      f.onComplete {
+        case Failure(t) => fail("Future failed")
+        case Success(i) =>
+          set += i -> ()
+          cdl.countDown()
+      }
+    }
+    assertTrue(cdl.await(5, TimeUnit.SECONDS))
+    assertEquals(futures.size, set.size)
   }
 
 }
