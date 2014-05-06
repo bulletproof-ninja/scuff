c0a0ff895b60638cac2f081821094182bb5ae13f
nilskp <nilskp@gmail.com>
2014-05-06 17:25:20 -0500

    Refactor

--- src/main/scala/scuff/MailRoom.scala
+++ src/main/scala/scuff/MailRoom.scala
@@ -1,30 +1,69 @@
 package scuff
 
-import java.io.{BufferedWriter, ByteArrayOutputStream, File, FileInputStream, OutputStream, OutputStreamWriter}
-import java.net.{InetAddress, InetSocketAddress}
+import java.io.{ BufferedWriter, ByteArrayOutputStream, File, FileInputStream, OutputStream, OutputStreamWriter }
+import java.net.{ InetAddress, InetSocketAddress }
 
 import language.implicitConversions
 
 import javax.activation.DataHandler
-import javax.mail.{Address, Message, MessagingException, PasswordAuthentication, Session, Transport}
-import javax.mail.internet.{InternetAddress, MimeBodyPart, MimeMessage, MimeMultipart}
+import javax.mail.{ Address, Message, MessagingException, PasswordAuthentication, Session, Transport }
+import javax.mail.internet.{ InternetAddress, MimeBodyPart, MimeMessage, MimeMultipart }
 import javax.mail.util.ByteArrayDataSource
 
-class MailRoom(session: Session, headers: (String, String)*) {
-  def this(smtpServer: InetSocketAddress, username: String, password: String, headers: (String, String)*) = this(MailRoom.makeSession(smtpServer, Some((username, password))), headers: _*)
-  def this(smtpServer: InetSocketAddress, headers: (String, String)*) = this(MailRoom.makeSession(smtpServer, None), headers: _*)
-
-  import MailRoom._
+trait MailRoom {
+  import MailRoom.Attachment
 
   /**
    * Send email.
    * @param from The sender
    * @param subject The email header
    * @param body The email body
-    * @param toAll The recipient(s)
+   * @param toAll The recipient(s)
    * @throws MessagingException If email could not be sent
    */
   @throws(classOf[MessagingException])
+  def send(from: InternetAddress, subject: String, body: Document, toAll: Iterable[InternetAddress], attachments: Attachment*)
+
+  /**
+   * Send email.
+   * @param from The sender
+   * @param subject The email header
+   * @param body The email body
+   * @param to The recipient
+   * @param toMore More recipients
+   * @throws MessagingException If email could not be sent
+   */
+  @throws(classOf[MessagingException])
+  final def send(from: InternetAddress, subject: String, body: Document, to: InternetAddress, toMore: InternetAddress*) {
+    send(from, subject, body, Seq(to) ++ toMore)
+  }
+
+}
+
+private class JavaxMailRoom(session: Session, headers: (String, String)*) extends MailRoom {
+
+  import MailRoom.Attachment
+
+  private def toDataHandler(doc: Document) = {
+    val out = new ByteArrayOutputStream
+    val wrt = new BufferedWriter(new OutputStreamWriter(out, doc.encoding))
+    doc.dump(wrt)
+    wrt.close()
+    val contentType = new javax.mail.internet.ContentType(doc.mimeType)
+    contentType.getParameter("charset") match {
+      case null ⇒ contentType.setParameter("charset", doc.encoding)
+      case cs ⇒ require(cs.toUpperCase == doc.encoding.toUpperCase, "MIME-type charset and encoding do not match: %s vs. %s".format(cs, doc.encoding))
+    }
+    new DataHandler(new ByteArrayDataSource(out.toByteArray, contentType.toString))
+  }
+  private def toDataHandler(doc: Attachment) = {
+    val out = new ByteArrayOutputStream
+    doc.dump(out)
+    out.close()
+    val contentType = new javax.mail.internet.ContentType(doc.mimeType)
+    new DataHandler(new ByteArrayDataSource(out.toByteArray, contentType.toString))
+  }
+
   def send(from: InternetAddress, subject: String, body: Document, toAll: Iterable[InternetAddress], attachments: Attachment*) {
     val msg = new MimeMessage(session)
     msg.setFrom(from)
@@ -46,50 +85,24 @@ class MailRoom(session: Session, headers: (String, String)*) {
     }
     Transport.send(msg)
   }
-  /**
-    * Send email.
-    * @param from The sender
-    * @param subject The email header
-    * @param body The email body
-    * @param to The recipient
-    * @param toMore More recipients
-    * @throws MessagingException If email could not be sent
-    */
-  @throws(classOf[MessagingException])
-  def send(from: InternetAddress, subject: String, body: Document, to: InternetAddress, toMore: InternetAddress*) {
-    send(from, subject, body, Seq(to) ++ toMore)
-  }
 
 }
 
 object MailRoom {
 
-  implicit def toInternetAddress(email: EmailAddress) = new InternetAddress(email.toString, true)
-  implicit def toInternetAddress(email: String) = new InternetAddress(email, true)
-  implicit def toSocketAddress(addr: InetAddress) = new InetSocketAddress(addr, DefaultSmtpPort)
-  implicit def toSocketAddress(addr: String) = new InetSocketAddress(addr, DefaultSmtpPort)
+  object Implicits {
+    implicit def toInternetAddress(email: EmailAddress) = new InternetAddress(email.toString, true)
+    implicit def toInternetAddress(email: String) = new InternetAddress(email, true)
+    implicit def toSocketAddress(addr: InetAddress) = new InetSocketAddress(addr, DefaultSmtpPort)
+  }
 
   private final val DefaultSmtpPort = 25
 
-  private def toDataHandler(doc: Document) = {
-    val out = new ByteArrayOutputStream
-    val wrt = new BufferedWriter(new OutputStreamWriter(out, doc.encoding))
-    doc.dump(wrt)
-    wrt.close()
-    val contentType = new javax.mail.internet.ContentType(doc.mimeType)
-    contentType.getParameter("charset") match {
-      case null ⇒ contentType.setParameter("charset", doc.encoding)
-      case cs ⇒ require(cs.toUpperCase == doc.encoding.toUpperCase, "MIME-type charset and encoding do not match: %s vs. %s".format(cs, doc.encoding))
-    }
-    new DataHandler(new ByteArrayDataSource(out.toByteArray, contentType.toString))
-  }
-  private def toDataHandler(doc: Attachment) = {
-    val out = new ByteArrayOutputStream
-    doc.dump(out)
-    out.close()
-    val contentType = new javax.mail.internet.ContentType(doc.mimeType)
-    new DataHandler(new ByteArrayDataSource(out.toByteArray, contentType.toString))
-  }
+  def apply(smtpServer: InetSocketAddress, username: String, password: String, headers: (String, String)*): MailRoom =
+    new JavaxMailRoom(makeSession(smtpServer, Some((username, password))), headers: _*)
+
+  def apply(smtpServer: InetSocketAddress, headers: (String, String)*): MailRoom =
+    new JavaxMailRoom(makeSession(smtpServer, None), headers: _*)
 
   def makeSession(smtpServer: InetSocketAddress, userPass: Option[(String, String)], otherProps: Map[String, Any] = Map.empty) = {
     val props = new java.util.Properties
@@ -114,7 +127,11 @@ object MailRoom {
     def dump(out: OutputStream)
     def mimeType: String
   }
-  class DocAttachment(doc: Document, val name: String) extends Attachment {
+  object Attachment {
+    def apply(doc: Document, name: String): Attachment = new DocAttachment(doc, name)
+    def apply(file: File, mimeType: String, altName: String = null): Attachment = new FileAttachment(file, mimeType, Option(altName))
+  }
+  private class DocAttachment(doc: Document, val name: String) extends Attachment {
     require(name != null, "Must have document name")
     def dump(out: OutputStream) {
       val writer = new OutputStreamWriter(out)
@@ -123,11 +140,15 @@ object MailRoom {
     }
     def mimeType: String = doc.mimeType
   }
-  class FileAttachment(file: File, val mimeType: String, altName: String = null) extends Attachment {
-    val name = Option(altName).getOrElse(file.getName)
+  private class FileAttachment(file: File, val mimeType: String, altName: Option[String]) extends Attachment {
+    val name = altName.getOrElse(file.getName)
     def dump(out: OutputStream) {
       val is = new FileInputStream(file)
-      IO.copyStream(is -> out)
+      try {
+        IO.copyStream(is -> out)
+      } finally {
+        is.close()
+      }
     }
   }
 
