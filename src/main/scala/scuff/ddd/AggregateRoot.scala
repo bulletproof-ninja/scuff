85db584a0a23781effd691c18013869f13c976eb
nilskp <nilskp@gmail.com>
2014-08-09 13:40:14 -0500

    Fix hashCode

--- src/main/scala/scuff/ddd/AggregateRoot.scala
+++ src/main/scala/scuff/ddd/AggregateRoot.scala
@@ -1,16 +1,18 @@
 package scuff.ddd
 
 trait AggregateRoot {
-  override final lazy val hashCode = id.hashCode ^ revision.map(_.asInstanceOf[Int]).getOrElse(17)
+  override final lazy val hashCode = id.hashCode ^ revision.getOrElse(-1)
   override final def equals(obj: Any) = obj match {
     case that: AggregateRoot ⇒ this.id == that.id && this.revision == that.revision
     case _ ⇒ false
   }
 
-  type EVT <: DomainEvent
+  /** ID type. */
   type ID
   /** Unique identifier. */
   def id: ID
+  /** Domain event type. */
+  type EVT <: DomainEvent
   /** Current revision. */
   def revision: Option[Int]
   /** Events produced. */
