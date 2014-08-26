ff1cbbe0ba8f9aa4aa541c7a7ba4431ee7073e6f
nilskp <nilskp@gmail.com>
2014-08-26 15:10:54 -0500

    Few more test cases added.

--- src/test/scala/scuff/TestScuffString.scala
+++ src/test/scala/scuff/TestScuffString.scala
@@ -17,4 +17,20 @@ class TestScuffString {
     assertFalse("abcdefg".offsetStartsWith(1, "abcdef"))
     assertTrue("abcdefg".offsetStartsWith(0, "abcdef"))
   }
+
+  @Test
+  def parseInt {
+    assertEquals(45678, "45678".unsafeInt(offset = 0))
+    assertEquals(45678, "45678".unsafeInt(offset = 0, length = 5))
+    assertEquals(45678, "abc45678".unsafeInt(offset = 3))
+    assertEquals(45678, "abc45678def".unsafeInt(offset = 3, length = 5))
+  }
+
+  @Test
+  def parseLong {
+    assertEquals(45678L, "45678".unsafeLong(offset = 0))
+    assertEquals(45678L, "45678".unsafeLong(offset = 0, length = 5))
+    assertEquals(45678L, "abc45678".unsafeLong(offset = 3))
+    assertEquals(45678L, "abc45678def".unsafeLong(offset = 3, length = 5))
+  }
 }
