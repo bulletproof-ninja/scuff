cef8a0564652be76e39678ad5fae32c67c8aaea0
nilskp <nilskp@gmail.com>
2014-09-01 17:52:24 -0500

    Refactor

--- src/main/scala/scuff/mongo/MongoDistributedLockStore.scala
+++ src/main/scala/scuff/mongo/MongoDistributedLockStore.scala
@@ -9,36 +9,46 @@ import java.net.InetSocketAddress
 import scuff.Timestamp
 import scala.util._
 import com.mongodb.DuplicateKeyException
+import com.mongodb.DBObject
 
-class MongoLockStore[ID](lockCollection: DBCollection)(implicit codec: Codec[ID, BsonValue]) extends LockStore[ID] {
+final class MongoLockStore[ID](lockCollection: DBCollection)(implicit codec: Codec[ID, BsonValue]) extends LockStore[ID] {
 
-  private def newDoc(key: ID, address: InetSocketAddress) = obj(
+  private def newDoc(key: ID, record: Record) = obj(
     "_id" := key,
-    "ip" := address.getAddress.getHostAddress,
-    "port" := address.getPort
+    "ip" := record.socketAddress.getAddress.getHostAddress,
+    "port" := record.socketAddress.getPort,
+    "jvm" := record.jvm,
+    "since" := record.since
   )
 
-  final def lock(key: ID, address: InetSocketAddress): Option[ExistingLock] = {
-    val lockDoc = newDoc(key, address).add("since" := new Timestamp)
+  private def toRecord(doc: DBObject): Record = {
+    val host = InetAddress.getByName(doc("ip").as[String])
+    val port = doc("port").as[Int]
+    val since = doc("since").as[Timestamp]
+    val address = new InetSocketAddress(host, port)
+    val jvm = doc("jvm").as[String]
+    Record(address, since, jvm)
+  }
+
+  def isLocked(key: ID): Option[Record] = lockCollection.findOpt(_id(key)).map(toRecord)
+
+  final def writeLock(key: ID, record: Record): Option[Record] = {
+    val lockDoc = newDoc(key, record)
     Try(lockCollection.safeInsert(lockDoc)) match {
       case Failure(_: DuplicateKeyException) =>
         lockCollection.findOpt(_id(key)) match {
           case None =>
-            lock(key, address) // Try again
+            writeLock(key, record) // Try again
           case Some(doc) =>
-            val host = InetAddress.getByName(doc("ip").as[String])
-            val port = doc("port").as[Int]
-            val since = doc("since").as[Timestamp]
-            val address = new InetSocketAddress(host, port)
-            Some(ExistingLock(address, since))
+            Some(toRecord(doc))
         }
       case Failure(other) => throw other
       case Success(_) => None
     }
   }
 
-  def unlock(key: ID, address: InetSocketAddress) {
-    lockCollection.remove(newDoc(key, address))
+  def removeLock(key: ID, record: Record) {
+    lockCollection.remove(newDoc(key, record))
   }
 
 }
