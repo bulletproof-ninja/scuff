25c5946272bdd85529a31b5da7b699ef2a503bfc
nilskp <nilskp@gmail.com>
2014-09-15 09:15:44 -0500

    refactor of exception handling

--- src/main/scala/scuff/redis/package.scala
+++ src/main/scala/scuff/redis/package.scala
@@ -1,14 +1,19 @@
 package scuff
 
+import _root_.redis.clients.util.SafeEncoder
 import _root_.redis.clients.jedis._
 import _root_.redis.clients.util.Pool
-
 import language.implicitConversions
+import scala.util.Try
 
 /**
  * This package requires the Jedis project.
  */
 package object redis {
+
+  implicit def toLeftJedis(jedis: BinaryJedis): Either[BinaryJedis, Pipeline] = Left(jedis)
+  implicit def toRightPipeline(pl: Pipeline): Either[BinaryJedis, Pipeline] = Right(pl)
+
   implicit def uri2Info(uri: java.net.URI) = {
     val userInfo: Option[(String, Option[String])] = Option(uri.getUserInfo).map { userInfo ⇒
       userInfo.split(":") match {
@@ -36,23 +41,23 @@ package object redis {
     new JedisPool(config, info.getHost, info.getPort, Protocol.DEFAULT_TIMEOUT, info.getPassword, db.getOrElse(0), name)
   }
 
-  implicit final class ScuffJedis(val jedis: Jedis) extends AnyVal {
+  implicit final class ScuffJedis(val jedis: BinaryJedis) extends AnyVal {
     /**
      * NOTICE: The transaction block may be executed multiple times,
      * so make sure it's idempotent.
      */
-    def transaction[T](keys: String*)(block: Transaction ⇒ T): T = {
-      if (keys.nonEmpty) jedis.watch(keys: _*)
+    def transaction[T](watch: String*)(block: Transaction ⇒ T): T = {
+      if (watch.nonEmpty) jedis.watch(watch.map(SafeEncoder.encode): _*)
       val txn = jedis.multi()
-      try {
-        val t = block(txn)
-        if (txn.exec == null) {
-          transaction(keys: _*)(block)
-        } else {
-          t
-        }
+      val t = try {
+        block(txn)
       } catch {
-        case e: Exception ⇒ try { txn.discard() } catch { case _: Exception ⇒ /* Ignore */ }; throw e
+        case e: Exception ⇒ Try(txn.discard); throw e
+      }
+      if (txn.exec() == null) {
+        transaction(watch: _*)(block)
+      } else {
+        t
       }
     }
 
