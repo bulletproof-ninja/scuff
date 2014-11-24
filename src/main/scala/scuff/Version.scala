c41c403305d57081d2247655ff1a6aefaf16492c
nilskp <nilskp@gmail.com>
2014-11-24 17:07:35 -0600

    docs

--- src/main/scala/scuff/Version.scala
+++ src/main/scala/scuff/Version.scala
@@ -1,14 +1,14 @@
 package scuff
 
 /**
-  * Trait to provide versioning to classes.
+  * Trait to provide versioning to types.
   */
 trait Version {
   def typeVersion: Short
 }
 
 /**
-  * Trait to provide an upgrade path to versioned classes.
+  * Trait to provide an upgrade path to versioned types.
   */
 trait ReplacedBy[T <: Version] extends Version {
   def upgrade(): T
