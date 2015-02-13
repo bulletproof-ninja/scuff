c155d5bd430d0317595ce2dda03a28d3137d200c
nilskp <nilskp@gmail.com>
2015-02-13 12:58:10 -0600

    add default precision as precision of clock itself.

--- src/main/scala/scuff/Clock.scala
+++ src/main/scala/scuff/Clock.scala
@@ -4,7 +4,7 @@ import java.util.concurrent.TimeUnit
 
 trait Clock {
   def precision: TimeUnit
-  def now(implicit toPrecision: TimeUnit): Long
+  def now(implicit toPrecision: TimeUnit = precision): Long
   @inline final def timestamp = new Timestamp(now(TimeUnit.MILLISECONDS))
   @inline final def durationSince(earlier: Long)(implicit toPrecision: TimeUnit): Long = now - earlier
   @inline final def durationUntil(future: Long)(implicit toPrecision: TimeUnit): Long = future - now
