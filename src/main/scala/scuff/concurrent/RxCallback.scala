2383c11e473a6f6c609e3adb6a392a8176e8b036
nilskp <nilskp@gmail.com>
2015-04-12 10:56:23 -0500

    Refactor.

--- src/main/scala/scuff/concurrent/RxCallback.scala
+++ src/main/scala/scuff/concurrent/RxCallback.scala
@@ -1,4 +1,4 @@
-package scuff
+package scuff.concurrent
 
 trait RxCallback[T] {
   def onNext(t: T): Unit
@@ -9,34 +9,33 @@ trait RxCallback[T] {
 import scala.concurrent._
 import scala.util.Try
 
+trait RxFuture[V, +R] extends RxCallback[V] {
+
+  private[this] val promise = Promise[R]
+  final def future = promise.future
+  final def onError(t: Throwable): Unit = promise failure t
+  final def onCompleted(): Unit = promise complete Try(result)
+
+  protected def result(): R
+
+}
+
 object RxFuture {
   def fold[V, R](subscribe: RxCallback[V] => Unit)(init: R)(f: (R, V) => R): Future[R] = {
     val callback = new RxFuture[V, R] {
       private[this] var acc = init
       def onNext(value: V): Unit = acc = f(acc, value)
-      protected def whenCompleted(): R = acc
+      protected def result(): R = acc
     }
     subscribe(callback)
     callback.future
   }
   def apply[V](thunk: V => Unit) = new RxFuture[V, Unit] {
     def onNext(value: V) = thunk(value)
-    protected def whenCompleted() = ()
+    protected def result() = ()
   }
   def apply[V, R](result: R)(thunk: V => Unit) = new RxFuture[V, R] {
     def onNext(value: V) = thunk(value)
-    protected def whenCompleted() = result
+    protected def result() = result
   }
-}
-
-abstract class RxFuture[V, +R] extends RxCallback[V] {
-
-  private[this] val promise = Promise[R]
-  final def future = promise.future
-  final def onError(t: Throwable): Unit = promise failure t
-  final def onCompleted(): Unit = promise complete Try(whenCompleted)
-
-  def onNext(value: V): Unit
-  protected def whenCompleted(): R
-
 }
