06ab81275cd3555a1b724a37342fb09165e68f55
nilskp <nilskp@gmail.com>
2016-11-04 10:18:25 -0500

    Delete all Jedis dependencies

--- redis/src/main/scala/scuff/redis/RedisCache.scala
+++
@@ -1,6 +0,0 @@
-package scuff.redis
-
-import scala.concurrent.duration.FiniteDuration
-
-class RedisCache(defaultTTL: FiniteDuration, conn: CONNECTION)
-    extends BinaryRedisCache(defaultTTL, conn, StringSerializer, StringSerializer)
