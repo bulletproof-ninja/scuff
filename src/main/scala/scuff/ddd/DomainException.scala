b094e876414a2ee02968db855226cb4d0f6db04a
nilskp <nilskp@gmail.com>
2016-12-08 14:12:34 -0600

    Remove, not useful.

--- src/main/scala/scuff/ddd/DomainException.scala
+++
@@ -1,6 +0,0 @@
-package scuff.ddd
-
-class DomainException(val reason: String, val parms: Any*) extends RuntimeException(reason) {
-  def toString(fmt: scuff.L10nPropFormatter) = fmt(reason, parms)
-  override def toString = reason
-}
