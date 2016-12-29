917dd4b6677bd7e112b9c2a62f606e56f5e1d5f0
nilskp <nilskp@gmail.com>
2016-12-29 14:40:06 -0600

    Move StreamPromise to separate file.

--- src/main/scala/scuff/concurrent/StreamCallback.scala
+++ src/main/scala/scuff/concurrent/StreamCallback.scala
@@ -1,45 +1,7 @@
 package scuff.concurrent
 
-import scala.concurrent._
-import scala.util.Try
-
 trait StreamCallback[T] {
   def onNext(t: T): Unit
   def onError(t: Throwable): Unit
   def onCompleted(): Unit
-}
-
-trait StreamPromise[V, +R] extends StreamCallback[V] {
-
-  private[this] val promise = Promise[R]
-  final def future = promise.future
-  final def onError(t: Throwable): Unit = promise failure t
-  final def onCompleted(): Unit = promise complete Try(result)
-
-  protected def result(): R
-
-}
-
-object StreamPromise {
-
-  def fold[V, R](subscribe: StreamCallback[V] => Unit)(init: R)(f: (R, V) => R): Future[R] = {
-    val callback = new StreamPromise[V, R] {
-      private[this] var acc = init
-      def onNext(value: V): Unit = acc = f(acc, value)
-      protected def result(): R = acc
-    }
-    subscribe(callback)
-    callback.future
-  }
-  def foreach[V](subscribe: StreamCallback[V] => Unit)(next: V => Unit): Future[Unit] = {
-    val callback = StreamPromise(next)
-    subscribe(callback)
-    callback.future
-  }
-
-  def apply[V](next: V => Unit): StreamPromise[V, Unit] = apply(())(next)
-  def apply[V, R](lazyResult: => R)(next: V => Unit) = new StreamPromise[V, R] {
-    def onNext(value: V) = next(value)
-    protected def result() = lazyResult
-  }
 }
