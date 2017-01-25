9a41f51a48db7258207fb7e06dbbdac278266607
nilskp <nilskp@gmail.com>
2017-01-25 13:34:08 -0600

    Update docs.

--- src/main/scala/scuff/Password.scala
+++ src/main/scala/scuff/Password.scala
@@ -5,23 +5,24 @@ import java.nio.charset.Charset
 import scala.concurrent.duration._
 
 /**
- * Password representation that stores a digested, non-reversible,
- * version of a password.
- * <p>This class is immutable.
- * <p>See <a href="http://java.sun.com/j2se/1.5.0/docs/guide/security/CryptoSpec.html#AppA">
- * Java Cryptography Reference</a> for standard names of digest algorithms.
- * <p>A String password is digested in the following manner:
- *   1. Convert to bytes using UTF-8 encoding
- *   2. Append salt
- *   3. Digest
- *   4. For workFactor > 1, repeat from 2, using the digest output as input
- * @author Nils Kilden-Pedersen
- * @see java.security.MessageDigest
- */
-final class Password(passwordDigest: Array[Byte], val algorithm: String, saltBytes: Array[Byte], val workFactor: Int) extends Serializable {
+  * Password representation that stores a digested, non-reversible,
+  * version of a password.
+  * <p>This class is immutable.
+  * <p>See <a href="http://java.sun.com/j2se/1.5.0/docs/guide/security/CryptoSpec.html#AppA">
+  * Java Cryptography Reference</a> for standard names of digest algorithms.
+  * <p>A String password is digested in the following manner:
+  *   1. Convert to bytes using UTF-8 encoding
+  *   2. Append salt
+  *   3. Digest
+  *   4. For workFactor > 1, repeat from 2, using the digest output as input
+  * @author Nils Kilden-Pedersen
+  * @see java.security.MessageDigest
+  */
+final class Password(passwordDigest: Array[Byte], val algorithm: String, saltBytes: Array[Byte], val workFactor: Int)
+    extends Serializable {
   require(passwordDigest != null, "Digest cannot be null")
   require(algorithm != null, "Algorithm cannot be null")
-  require(workFactor > 0, "Must have a work factor of at least one, not " + workFactor)
+  require(workFactor > 0, s"Must have a work factor of at least one, not $workFactor")
 
   private val _digest = passwordDigest.clone // Defensive copy
   private val _salt = saltBytes.clone // Defensive copy
@@ -52,10 +53,10 @@ final class Password(passwordDigest: Array[Byte], val algorithm: String, saltByt
   override val hashCode = Arrays.hashCode(passwordDigest)
 
   /**
-   * Check if password string matches this password.
-   * @param Clear text password
-   * @return `true` if it's a match
-   */
+    * Check if password string matches this password.
+    * @param Clear text password
+    * @return `true` if it's a match, `false` otherwise
+    */
   def matches(password: String) = {
     val (compareDigest, _) = Password.digestion(password, _salt, algorithm, Left(workFactor))
     Arrays.equals(_digest, compareDigest)
@@ -117,13 +118,13 @@ object Password {
   }
 
   /**
-   * Configuration.
-   * @param algorithm The digest algorithm. This string must be understood by [[java.security.MessageDigest]]
-   * @param saltLength The length of the random salt generated. Can be 0 for no salt.
-   * @param iterative Defines how to digest iteratively. Can be either a fixed number (at least 1),
-   * or a minimum duration (don't go overboard here). Using a duration will be adaptive to the hardware
-   * it's running on, and makes digestion time more predictable.
-   */
+    * Configuration.
+    * @param algorithm The digest algorithm. This string must be understood by [[java.security.MessageDigest]]
+    * @param saltLength The length of the random salt generated. Can be 0 for no salt.
+    * @param work Defines how to digest iteratively. Can be either a fixed number (at least 1),
+    * or a minimum duration (don't go overboard here). Using a duration will be adaptive to the hardware
+    * it's running on, and makes digestion time more predictable.
+    */
   case class Config(algorithm: String, saltLength: Int, work: Either[Int, FiniteDuration]) {
     require(MessageDigest.getInstance(algorithm) != null)
     require(saltLength >= 0, "Negative salt length is nonsensical")
@@ -132,16 +133,16 @@ object Password {
       case Right(duration) => require(duration.isFinite, "Must be a finite duration: " + duration)
     }
     /**
-     * @param algorithm The digest algorithm. This string must be understood by [[java.security.MessageDigest]]
-     * @param saltLength The length of the random salt generated. Can be 0 for no salt.
-     * @param workFactor Number of times to iterate the digest. Must be at least 1.
-     */
+      * @param algorithm The digest algorithm. This string must be understood by [[java.security.MessageDigest]]
+      * @param saltLength The length of the random salt generated. Can be 0 for no salt.
+      * @param workFactor Number of times to iterate the digest. Must be at least 1.
+      */
     def this(algorithm: String, saltLength: Int, workFactor: Int) = this(algorithm, saltLength, Left(workFactor))
     /**
-     * @param algorithm The digest algorithm. This string must be understood by [[java.security.MessageDigest]]
-     * @param saltLength The length of the random salt generated. Can be 0 for no salt.
-     * @param digestDuration Minimum amount of time spent on iterative digestion
-     */
+      * @param algorithm The digest algorithm. This string must be understood by [[java.security.MessageDigest]]
+      * @param saltLength The length of the random salt generated. Can be 0 for no salt.
+      * @param workDuration Minimum amount of time spent on iterative digestion
+      */
     def this(algorithm: String, saltLength: Int, workDuration: FiniteDuration) = this(algorithm, saltLength, Right(workDuration))
   }
 
