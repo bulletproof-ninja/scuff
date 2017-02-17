3f2fb50b8547c1bb8cee0afe96659adbc6b1f73f
nilskp <nilskp@gmail.com>
2017-02-17 12:15:06 -0600

    Improve exception messages for missing/invalid values

--- src/main/scala/scuff/Props.scala
+++ src/main/scala/scuff/Props.scala
@@ -3,16 +3,16 @@ package scuff
 /**
   * Look up properties from generic source, with generic fallback.
   */
-class Props(getProperty: String => String, fallback: Props = null) {
+class Props(metaName: String, getProperty: String => String, fallback: Props = null) {
   def optional(name: String, validValues: Set[String] = Set.empty): Option[String] = {
     var value = getProperty(name)
     if (value == null && fallback != null) {
-      value = fallback.optional(name).orNull
+      value = fallback.optional(name, validValues).orNull
     }
     if (value == null) {
       None
     } else if (!validValues.isEmpty && !validValues.contains(value)) {
-      throw new IllegalStateException("Property '%s' value '%s' not among valid values: %s".format(name, value, validValues.mkString("[", ", ", "]")))
+      throw new IllegalStateException(s"The $metaName '$name' has invalid value '$value'; valid values: [${validValues.mkString(", ")}]")
     } else {
       Some(value)
     }
@@ -20,16 +20,16 @@ class Props(getProperty: String => String, fallback: Props = null) {
 
   @throws(classOf[IllegalStateException])
   def required(name: String, validValues: Set[String] = Set.empty): String = optional(name, validValues) match {
-    case None if validValues.isEmpty => throw new IllegalStateException("Required system property missing: " + name)
-    case None => throw new IllegalStateException("Required property missing: %s, valid values: %s".format(name, validValues.mkString("[", ", ", "]")))
+    case None if validValues.isEmpty => throw new IllegalStateException(s"Required $metaName '$name' missing")
+    case None => throw new IllegalStateException(s"Required $metaName '$name' missing; valid values: [${validValues.mkString(", ")}]")
     case Some(value) => value
   }
 
 }
 
-class SysProps(fallback: Props) extends Props(System.getProperty, fallback)
+class SysProps(fallback: Props) extends Props("system property", System.getProperty, fallback)
 object SysProps extends SysProps(null)
-class EnvVars(fallback: Props) extends Props(System.getenv, fallback)
+class EnvVars(fallback: Props) extends Props("environment variable", System.getenv, fallback)
 object EnvVars extends EnvVars(null)
 
 object Props {
@@ -40,6 +40,6 @@ object Props {
     require(file.canRead, "Cannot read: " + file)
     val props = new java.util.Properties
     props.load(new FileReader(file))
-    new Props(props.getProperty, fallback)
+    new Props(s"${file.getName} property", props.getProperty, fallback)
   }
 }
---
+++ src/test/scala/scuff/TestProps.scala
@@ -0,0 +1,63 @@
+package scuff
+
+import org.junit._
+import org.junit.Assert._
+import java.io.File
+
+class TestProps {
+  @Test(expected = classOf[IllegalStateException])
+  def `invalid value` {
+    System.setProperty("foo", "baz")
+    SysProps.optional("foo", Set("foo", "bar"))
+    fail("Should have failed on 'baz'")
+  }
+
+  @Test
+  def `valid value` {
+    System.setProperty("foo", "baz")
+    assertEquals("baz", SysProps.required("foo", Set("foo", "bar", "baz")))
+  }
+
+  @Test
+  def `error message` {
+    System.setProperty("foo", "baz")
+    try {
+      SysProps.required("foo", Set("foo", "bar"))
+    } catch {
+      case e: Exception => println(e.getMessage)
+    }
+  }
+
+  @Test
+  def `env fallback` {
+    val props = new SysProps(EnvVars)
+    System.getenv("JAVA_HOME") match {
+      case null => assertEquals(None, props.optional("JAVA_HOME"))
+      case javaHome =>
+        assertEquals(javaHome, props.required("JAVA_HOME"))
+        try {
+          props.required("JAVA_HOME", Set("hahaha"))
+          fail("Should fail because not funny")
+        } catch {
+          case e: IllegalStateException =>
+            assertTrue(e.getMessage.contains("environment variable"))
+        }
+    }
+  }
+
+  @Test
+  def from_file {
+    val file = new File(getClass.getResource("/FooBar.properties").getFile)
+    println(file.getAbsolutePath)
+    val props = Props(file)
+    assertEquals("Hello", props.required("say"))
+    try {
+      props.required("blablabla")
+    } catch {
+      case e: IllegalStateException =>
+        println(e.getMessage)
+        assertTrue(e.getMessage.contains(file.getName))
+    }
+  }
+
+}
