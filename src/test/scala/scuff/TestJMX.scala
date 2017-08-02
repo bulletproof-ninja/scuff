7b116761271a416ad2339878ba9de0b4caae8940
nilskp <nilskp@gmail.com>
2017-08-02 11:09:08 -0500

    Refactor tests to verify conditional name quoting

--- src/test/scala/scuff/TestJMX.scala
+++ src/test/scala/scuff/TestJMX.scala
@@ -4,6 +4,7 @@ import javax.management.MXBean
 import scala.beans.BeanProperty
 
 import org.junit._, Assert._
+import javax.management.ObjectName
 
 object TestJMX {
   trait FooBeanMXBean {
@@ -21,20 +22,28 @@ object TestJMX {
 class TestJMX {
   import TestJMX._
 
-  private def uniqueNames(beanName: String, typeName: String, i1: AnyRef, i2: AnyRef) {
+  private def uniqueNames(quoted: Boolean, beanName: String, typeName: String, i1: AnyRef, i2: AnyRef) {
     val objName1 = JMX.register(i1, beanName)
-    assertEquals(beanName, objName1.getKeyProperty("name"))
+    if (quoted) assertEquals(ObjectName quote beanName, objName1.getKeyProperty("name"))
+    else assertEquals(beanName, objName1.getKeyProperty("name"))
     val objName2 = JMX.register(i2, beanName)
-    assertEquals(s"$beanName[1]", objName2.getKeyProperty("name"))
+    if (quoted) assertEquals(ObjectName quote s"$beanName[1]", objName2.getKeyProperty("name"))
+    else assertEquals(s"$beanName[1]", objName2.getKeyProperty("name"))
 
     assertEquals(typeName, objName1.getKeyProperty("type"))
     assertEquals(typeName, objName2.getKeyProperty("type"))
   }
 
   @Test
-  def uniqueNames_foo() = uniqueNames("MyFoo", "FooBean", FooBeanCaseClassImpl(5), FooBeanCaseClassImpl(7))
+  def uniqueNames_foo() = {
+    uniqueNames(false, "MyFoo", "FooBean", FooBeanCaseClassImpl(5), FooBeanCaseClassImpl(7))
+    uniqueNames(true, "My Foo", "FooBean", FooBeanCaseClassImpl(55), FooBeanCaseClassImpl(77))
+  }
   @Test
-  def uniqueNames_bar() = uniqueNames("MyBar", "BarBean", new BarBeanImpl("5"), BarBeanImpl("7"))
+  def uniqueNames_bar() = {
+    uniqueNames(false, "MyBar", "BarBean", new BarBeanImpl("5"), BarBeanImpl("7"))
+    uniqueNames(true, "My,Bar", "BarBean", new BarBeanImpl("55"), BarBeanImpl("77"))
+  }
 
   @Test
   def quoteUnquoted() {
@@ -42,4 +51,5 @@ class TestJMX {
     val objName = JMX.register(myBean, "My Bean")
     assertEquals("\"My Bean\"", objName.getKeyProperty("name"))
   }
+
 }
