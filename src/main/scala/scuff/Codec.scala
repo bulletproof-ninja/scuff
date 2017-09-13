b9ab06657808515491d4fb2510d25cf978c996bb
nilskp <nilskp@gmail.com>
2017-09-13 14:59:12 -0500

    Fix infinite recursion
    Note to self: Always run tests before committing. :-)

--- src/main/scala/scuff/Codec.scala
+++ src/main/scala/scuff/Codec.scala
@@ -9,9 +9,10 @@ trait Codec[A, B] extends Serializable {
   def encode(a: A): B
   def decode(b: B): A
 
-  val reverse: Codec[B, A] = new Codec[B, A] {
+  def reverse: Codec[B, A] = new Codec[B, A] {
     def encode(b: B): A = Codec.this.decode(b)
     def decode(a: A): B = Codec.this.encode(a)
+    override def reverse: Codec[A, B] = Codec.this
   }
 
 }
