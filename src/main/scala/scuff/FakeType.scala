092ee07c9903cfd077f4e5607094019556a4d852
nilskp <nilskp@gmail.com>
2017-09-24 12:49:38 -0500

    Add FakeType pattern

---
+++ src/main/scala/scuff/FakeType.scala
@@ -0,0 +1,43 @@
+package scuff
+
+/**
+  * Fake wrapper type. Provides type-safety without overhead.
+  * Canonical usage:
+  * {{{
+  *   package object foo {
+  *
+  *     sealed abstract class IntWrapper extends FakeType[Int]
+  *
+  *     // It's essential that FooInt (and BarInt) are
+  *     // explicitly given the abstract IntWrapper type,
+  *     // and not the anonymous type that would otherwise
+  *     // be inferred.
+  *     val FooInt: IntWrapper = new IntWrapper {
+  *       type Type = Int
+  *       def apply(int: Int): Type = int
+  *       def unwrap(foo: Type): Int = foo
+  *     }
+  *     type FooInt = FooInt.Type
+  *
+  *     val BarInt: IntWrapper = new IntWrapper {
+  *       type Type = Int
+  *       def apply(int: Int): Type = int
+  *       def unwrap(bar: Type): Int = bar
+  *     }
+  *     type BarInt = BarInt.Type
+  *
+  *     val foo: FooInt = 5 // Does not compile
+  *     val foo: FooInt = FooInt(5)
+  *     val bar: BarInt = foo // Does not compile
+  *     val bar = BarInt(5)
+  *
+  *   }
+  *
+  * }}}
+  * @see https://failex.blogspot.ch/2017/04/the-high-cost-of-anyval-subclasses.html
+  */
+abstract class FakeType[@specialized T] {
+  type Type
+  def apply(t: T): Type
+  def unwrap(t: Type): T
+}
---
+++ src/test/scala/scuff/TestFakeTypes.scala
@@ -0,0 +1,29 @@
+package scuff
+
+import org.junit._, Assert._
+
+class TestFakeTypes {
+  @Test
+  def foo() {
+    sealed abstract class LongWrapper extends FakeType[Long]
+
+    val FooID: LongWrapper = new LongWrapper {
+      type Type = Long
+      def apply(long: Long): Type = long
+      def unwrap(id: Type): Long = id
+    }
+    type FooID = FooID.Type
+
+    val BarID: LongWrapper = new LongWrapper {
+      type Type = Long
+      def apply(long: Long) = long
+      def unwrap(wrp: Type) = wrp
+    }
+    type BarID = BarID.Type
+
+    val foo: FooID = FooID(5L)
+    val bar: BarID = BarID(FooID unwrap foo)
+    assertEquals(foo, bar)
+    assertTrue(foo == bar)
+  }
+}
