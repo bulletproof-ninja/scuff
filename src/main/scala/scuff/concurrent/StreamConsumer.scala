9e8558bf42a0217493dc492c59dc7a72e3099e85
nilskp <nilskp@gmail.com>
2017-11-06 13:48:30 -0600

    Introduce async stream consumer
    If onNext calls are processed asynchrounously, the isDone Future should ensure those are completed.

--- src/main/scala/scuff/concurrent/StreamConsumer.scala
+++ src/main/scala/scuff/concurrent/StreamConsumer.scala
@@ -12,3 +12,43 @@ trait StreamCallback[-T] extends StreamConsumer[T, Unit] {
   def onDone() = Future(onCompleted)(Threads.PiggyBack)
   def onCompleted(): Unit
 }
+
+trait AsyncStreamConsumer[-T, +R]
+  extends StreamConsumer[T, R] {
+  self: (T => Future[R]) =>
+
+  import scala.concurrent.Promise
+  import scala.concurrent.duration.Duration
+  import java.util.concurrent.TimeoutException
+
+  /** Timeout on `onDone()` Future. */
+  protected def timeout: Duration
+
+  private[this] val semaphore = new java.util.concurrent.Semaphore(Int.MaxValue)
+  private[this] val promise = Promise[Unit]
+
+  def onNext(t: T): Unit = {
+    val future = apply(t)
+    if (!future.isCompleted) {
+      semaphore.tryAcquire()
+      future.onComplete(_ => semaphore.release)(Threads.PiggyBack)
+    }
+  }
+
+  def onError(th: Throwable): Unit = promise failure th
+
+  def whenDone(): Future[R]
+  def onDone(): Future[R] = {
+    val completed: Future[Unit] =
+      if (promise.isCompleted) promise.future
+      else Threads.newBlockingThread(s"Awaiting completion of ${getClass.getName}", promise) {
+        if (timeout.isFinite) {
+          val acquired = semaphore.tryAcquire(Int.MaxValue, timeout.length, timeout.unit)
+          if (!acquired) throw new TimeoutException(s"Timed out after $timeout awaiting completion of ${getClass.getName}")
+        } else {
+          semaphore.acquire(Int.MaxValue)
+        }
+      }
+    completed.flatMap(_ => whenDone())(Threads.PiggyBack)
+  }
+}
