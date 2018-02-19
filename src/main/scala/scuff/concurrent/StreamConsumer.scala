0a8df9abe564c62249569c27e37b4ba7eb19a8fc
Nils Kilden-Pedersen <nilskp@gmail.com>
2018-02-19 14:17:59 -0600

    Refactor
    Use of Promise is slightly confusing, as it's not used in some cases. What is needed is capture of exception, which is done by atomic reference instead.

--- src/main/scala/scuff/concurrent/StreamConsumer.scala
+++ src/main/scala/scuff/concurrent/StreamConsumer.scala
@@ -1,6 +1,10 @@
 package scuff.concurrent
 
+import java.util.concurrent.TimeoutException
+import java.util.concurrent.atomic.AtomicReference
 import scala.concurrent.Future
+import scala.concurrent.duration.Duration
+import scala.concurrent.duration.FiniteDuration
 
 trait StreamConsumer[@specialized(AnyRef, Int, Long, Float, Double) -T, +R] {
   def onNext(t: T): Unit
@@ -12,37 +16,37 @@ trait AsyncStreamConsumer[-T, +R]
   extends StreamConsumer[T, R] {
   self: (T => Future[Unit]) =>
 
-  import scala.concurrent.Promise
-  import scala.concurrent.duration.Duration
-  import java.util.concurrent.TimeoutException
-
   /** Max. allowed processing time to completion after `onDone()`. */
   protected def completionTimeout: FiniteDuration
+  /** Called when processing is fully completed. */
+  protected def whenDone(): Future[R]
 
   private[this] val semaphore = new java.util.concurrent.Semaphore(Int.MaxValue)
-  private[this] val promise = Promise[Unit]
+  private[this] val error = new AtomicReference[Throwable]
 
   def onNext(t: T): Unit = {
-    val future = apply(t)
+    val future: Future[Unit] = apply(t)
     if (!future.isCompleted) {
       semaphore.tryAcquire()
       future.onComplete(_ => semaphore.release)(Threads.PiggyBack)
     }
   }
 
-  def onError(th: Throwable): Unit = promise failure th
+  def onError(th: Throwable): Unit = error.weakCompareAndSet(null, th)
 
-  def whenDone(): Future[R]
   def onDone(): Future[R] = {
     val completed: Future[Unit] =
-      if (promise.isCompleted) promise.future
-      else Threads.newBlockingThread(s"Awaiting completion of ${getClass.getName}", promise) {
-        if (timeout.isFinite) {
-          val acquired = semaphore.tryAcquire(Int.MaxValue, timeout.length, timeout.unit)
-          if (!acquired) throw new TimeoutException(s"Timed out after $timeout awaiting completion of ${getClass.getName}")
-        } else {
-          semaphore.acquire(Int.MaxValue)
-        }
+      error.get match {
+        case null if semaphore.tryAcquire(Int.MaxValue) => // Fast case
+          Future successful (())
+        case null => // Slow case
+          Threads.newBlockingThread(s"Awaiting completion of ${getClass.getName}") {
+            val timeout = completionTimeout
+            if (!semaphore.tryAcquire(Int.MaxValue, timeout.length, timeout.unit)) {
+              throw new TimeoutException(s"Timed out after $timeout awaiting completion of ${getClass.getName}")
+            }
+          }
+        case th => Future failed th
       }
     completed.flatMap(_ => whenDone())(Threads.PiggyBack)
   }
