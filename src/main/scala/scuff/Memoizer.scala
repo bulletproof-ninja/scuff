c3b9e7aec1e300cd751e26e1b024305327627561
nilskp <nilskp@gmail.com>
2019-01-17 14:58:20 -0600

    Small refactor

--- src/main/scala/scuff/Memoizer.scala
+++ src/main/scala/scuff/Memoizer.scala
@@ -7,19 +7,15 @@ import scuff.concurrent.LockFreeConcurrentMap
 /**
   * Class that guarantees exactly one instance created per
   * argument (based on `equals` equality).
-  * NOTICE: [[scala.collection.concurrent.Map#getOrElse]] and
-  * [[scala.collection.concurrent.Map#putIfAbsent]] can provide
-  * similar behavior, but cannot guarantee a single instance per
-  * key, due to inherent race conditions.
+  * NOTICE: [[scala.collection.concurrent.Map#getOrElseUpdate]]
+  * can provide similar behavior, but cannot guarantee a single
+  * instance per key, due to inherent race conditions.
   */
 class Memoizer[A, R](make: A => R) {
 
   private val locks = new collection.concurrent.TrieMap[A, Lock]
-  private def synchronized(arg: A)(thunk: => R): R = {
-    val lock = locks.get(arg) getOrElse {
-      val lock = new ReentrantLock
-      locks.putIfAbsent(arg, lock) getOrElse lock
-    }
+  private def synch(arg: A)(thunk: => R): R = {
+    val lock = locks.getOrElseUpdate(arg, new ReentrantLock)
     lock.lock()
     try {
       thunk
@@ -34,7 +30,7 @@ class Memoizer[A, R](make: A => R) {
     map.get(arg) match {
       case Some(res) => res
       case None =>
-        synchronized(arg) {
+        synch(arg) {
           map.getOrElseUpdate(arg, make(arg))
         }
     }
